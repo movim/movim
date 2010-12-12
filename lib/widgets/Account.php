00e4ddc700025d3d7dc9dc23b17d1ec32d0046ea
Jaussoin Timothée <edhelas@movim.eu>
2010-12-12 15:57:34 +0100

    Adding account page (not actually working)

---
+++ lib/widgets/Account.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * @file Chat.php
+ * This file is part of MOVIM.
+ * 
+ * @brief The account creation
+ *
+ * @author Timothée Jaussoin <edhelas_at_g m a i l dot com>
+ *
+ * @version 1.0
+ * @date 20 October 2010
+ *
+ * Copyright (C)2010 MOVIM project
+ * 
+ * See COPYING for licensing information.
+ */
+
+class Account extends Widget
+{
+	private $user;
+	
+	function __construct(&$user)
+	{
+		$this->user = $user;
+	}
+	
+	function build()
+	{
+			$form = new Form();
+			$form->startForm(basename($_SERVER['PHP_SELF']));
+				$form->startFieldset(_('Jabber Account'));
+				$form->newline = true;
+					$form->textInput('account',_('Jabber Account'),false,'block required');
+					$form->textInput('password',_('Password'),false,'block required');
+				$form->newline = true;
+				$form->closeFieldset();
+				$form->insertBR();
+				$form->newline = false;
+				$form->submitButton();
+				$form->newline = true;
+				$form->resetButton();
+			$form->closeForm();
+
+			if(!$output = $form->getForm()) { die("error: " . $form->error); }
+			else { echo $output; }
+	}
+}
+
+?>
---
+++ themes/movim/account.tpl
@@ -0,0 +1,9 @@
+<?php /* -*- mode: html -*- */
+?>
+<div id="left">
+Create your own account !
+</div>
+<div id="center">
+  <?php $this->widget('Account');?>
+</div>
+
