497d6e5064b59e25059d03864b64a27bc54d1f61
Jaussoin Timothée <edhelas@movim.eu>
2011-03-27 15:10:28 +0200

    Clean deprecated LOC

--- lib/Cache.php
+++ lib/Cache.php
@@ -26,54 +26,7 @@ function movim_cache($key)
 	if(func_num_args() == 2) {
         if(!file_put_contents(BASE_PATH."/user/".$login."/cache/".$key, serialize($content))) {
             throw new MovimException(sprintf(t("Couldn't set cache file %s"), $key));
-        }/* else {
-        	file_put_contents(BASE_PATH."/user/".$login."/".$key, $content);
-        }*/
+        }
 	}
 }
-#doc
-#	classname:	Cache
-#	scope:		PUBLIC
-#
-#/doc
 
-/*class Cache 
-{
-	#	internal variables
-	private $_login;
-	#	Constructor
-	function __construct ()
-	{
-		# code...
-		$session = Session::getInstance();;
-		$this->_login = $session->getLogin();
-	}
-	###	
-	public function get()
-	{
-		if(file_exists(BASE_PATH."/user/".$this->_login."/cache.tmp")) {
-			$content = file_get_contents(BASE_PATH."/user/".$this->_login."/cache.tmp");
-		}
-		else {
-			$content = "User cache not found";
-		}
-		
-		return $content;	
-	}
-	
-	public function put($html)
-	{
-		if(file_exists(BASE_PATH."/user/".$this->_login."/cache.tmp")) {
-			$content = file_get_contents(BASE_PATH."/user/".$this->_login."/cache.tmp");
-			$content = $html."\n".$content;
-			$inF = fopen(BASE_PATH."/user/".$this->_login."/cache.tmp","w");
-			fwrite($inF,$content);
-			fclose($inF);
-		}
-		else {
-			$content = "User cache not found";
-		}
-	}
-
-}*/
-###
