8c165dac999a24a48f3d87260363cea62a7c37a1
Jaussoin Timothée <edhelas@movim.eu>
2011-04-08 16:07:23 +0200

    Clean movim-log line

--- lib/XMPPConnect.php
+++ lib/XMPPConnect.php
@@ -197,7 +197,6 @@ class XMPPConnect
 	public function handle($payload) {
 		$evt = new EventHandler();
 		if(isset($payload['vCard'])) { // Holy mackerel, that's a vcard!
-						movim_log($payload['from']);
 			if(!is_null($payload['from'])) {
 			   	Cache::handle("vcard".$payload["from"], $payload);
 				$evt->runEvent('vcardreceived', $payload);
