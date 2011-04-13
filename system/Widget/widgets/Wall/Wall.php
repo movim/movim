4df6b46f7c2f020f47c7a1954816f3b261b10280
Etenil <guillaume@etenil.net>
2011-04-13 14:55:16 +0100

    Corrected incorrect wall widget.

--- system/Widget/widgets/Wall/Wall.php
+++ system/Widget/widgets/Wall/Wall.php
@@ -3,7 +3,7 @@
 /**
  * @file Chat.php
  * This file is part of MOVIM.
- * 
+ *
  * @brief A jabber chat widget.
  *
  * @author Guillaume Pasquet <etenil@etenilsrealm.nl>
@@ -12,36 +12,30 @@
  * @date 20 October 2010
  *
  * Copyright (C)2010 MOVIM project
- * 
+ *
  * See COPYING for licensing information.
  */
 
 class Wall extends WidgetBase
 {
+
+    function WidgetLoad()
+    {
+    	$this->addcss('wall.css');
+    }
+
 	function build()
 	{
 		?>
 		<div id="wall">
 		    <div class="message">
 				<img class="avatar" width="70px" height="70px" alt="test">
-			
+
 		    	<div class="content">
-		    	Ceci est un message de test
+ 			<?php echo t("This is my custom Wall widget.");?>
 		    	</div>
 		    	<div class="comment">
 		    	Commentaire 1
-		    	</div>
-		    	
-		    	<div class="comment">
-		    	Commentaire 2
-		    	</div>
-		    	
-		    	<div class="comment">
-		    	Commentaire 3
-		    	</div>
-		    	
-		    	<div class="comment">
-		    	Commentaire 4
 		    	</div>
        		</div>
        	</div>
