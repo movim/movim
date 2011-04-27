949b3b197e166dd4f84b35017d28f8139fce1cb6
Etenil <guillaume@etenil.net>
2011-04-27 15:00:59 +0100

    Added PHP unit-test library.

---
+++ tests/php/Storage/TestStorage.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @file TestStorage.php
+ * This file is part of PROJECT.
+ *
+ * @brief Description
+ *
+ * @author Guillaume Pasquet <gpasquet@lewisday.co.uk>
+ *
+ * @version 1.0
+ * @date 27 April 2011
+ *
+ * Copyright (C)2011 Lewis Day Transport Plc.
+ *
+ * All rights reserved.
+ */
+class TestStorage
+{
+    function testStuff()
+    {
+        ut_assert(true);
+    }
+}
+
+?>
