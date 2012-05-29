4797aa44ebf05ffaface39c5814fe0a3b54b3b4d
Kilian Holzinger kilian.holzinger@gmail.com <>
2012-05-29 15:05:20 +0200

    Setting display errors back to 0 in image.php

--- image.php
+++ image.php
@@ -17,7 +17,7 @@
  */
 
 ini_set('log_errors', 1);
-ini_set('display_errors', 1);
+ini_set('display_errors', 0);
 ini_set('error_reporting', E_ALL ^ E_DEPRECATED ^ E_NOTICE);
 ini_set('error_log', 'log/php.log');
 
