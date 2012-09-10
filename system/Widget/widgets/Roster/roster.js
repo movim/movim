eb981a7490931bb5e2ad4465abd856a7f9191d6e
Christine Ho <nodpounod@gmail.com>
2012-09-10 20:13:42 +0200

    - Keyboard navigation in Roster widget

--- system/Widget/widgets/Roster/roster.js
+++ system/Widget/widgets/Roster/roster.js
@@ -72,8 +72,42 @@ function focusContact(){
 	}
 }
 
+function rosterNext(c){
+	if(c.className.lastIndexOf("focused")>-1){
+		found = true;
+	}
+	if(contact.nextSibling != null && contact.nextSibling.className.lastIndexOf("offline")<0){
+		contact = c.nextSibling;
+	}
+	else{
+		if(contact.parentNode.nextSibling!=null && contact.parentNode.nextSibling.nodeName!='#text'){
+			contact = c.parentNode.nextSibling.querySelectorAll("li")[0];
+		}
+		else{
+			lastContact = c;
+		}
+	}
+}
+function rosterPrevious(c){
+	if(c.className.lastIndexOf("focused")>-1){
+		found = true;
+	}
+	if(c.previousSibling == null || c.previousSibling.nodeName == "H1"){
+		if(c.parentNode.previousSibling.nodeName!='#text' && c.parentNode.previousSibling!=null){
+			last = c.parentNode.previousSibling.querySelectorAll("li").length - 1;
+			contact = c.parentNode.previousSibling.querySelectorAll("li")[last];
+			if((decallage = 2*c.offsetHeight) == 0)
+				decallage = 2*c.previousSibling.offsetHeight;
+		}
+	}
+	else{
+		contact = c.previousSibling;
+		if(0 == (decallage = c.offsetHeight))
+		decallage = c.previousSibling.offsetHeight;
+	}
+}
 
-function search(e){
+function rosterSearch(e){
 	rosterlist = document.querySelector('#rosterlist');
 	parents = rosterlist.querySelectorAll('li');
 	names = rosterlist.getElementsByTagName('span');
@@ -95,23 +129,28 @@ function search(e){
 				}
 			}
 		}
+		document.querySelector('#right').scrollTop = 0;
 	}
 	else{
 		if(e.keyCode == 13){ //key pressed is enter; launch chat
 			eval(focused.getElementsByTagName("div")[0].getAttribute("onclick"));
 		}
-		/*if(e.keyCode>36 && e.keyCode<41){ //key pressed is an arrow
+		if(e.keyCode>36 && e.keyCode<41){ //key pressed is an arrow
+			//lastContact = "";
 			//contact is the first contact of the list which is shown (already sorted)
 			contact = rosterlist.querySelectorAll("li[style='display: list-item; ']")[0];
 			//otherwise it is the first contact of the list
 			if(typeof contact === 'undefined'){
 				contact = rosterlist.querySelectorAll("li:not([class='offline '])")[0];
 			}
+			found = false;
+			if(0 == (decallage = contact.offsetHeight))
+				decallage = contact.nextSibling.offsetHeight;
+
 			switch(e.keyCode){
 				//previous
 				case e.keyCode = 37:
 				case e.keyCode = 38:
-					found = false;//the focused one
 					while(contact.className.lastIndexOf("focused") == -1){
 						if(contact.nextSibling != null){
 							contact = contact.nextSibling;
@@ -120,90 +159,43 @@ function search(e){
 							contact = contact.parentNode.nextSibling.querySelectorAll("li")[0];
 						}
 					}
-					found = false;
 					if(document.querySelector('.offline').style.display == ''){//before filtering, offline contacts are hidden
 						while((contact.className.lastIndexOf("offline")>-1 || contact.className.lastIndexOf("error")>-1) || !found){//so they can't be focused
-							if(contact.className.lastIndexOf("focused") > -1){
-								found = true;
-							}
-							if(contact.previousSibling == null || contact.previousSibling.nodeName == "H1"){
-								if(contact.parentNode.previousSibling.nodeName!='#text' && contact.parentNode.previousSibling!=null){
-									last = contact.parentNode.previousSibling.querySelectorAll("li").length - 1;
-									contact = contact.parentNode.previousSibling.querySelectorAll("li")[last];
-									decallage = 2*contact.offsetHeight;
-								}
-							}
-							else{
-								contact = contact.previousSibling;
-								decallage = contact.offsetHeight;
-							}
+							rosterPrevious(contact);
 						}
 					}
 					else{
 						while(contact.getAttribute('style').lastIndexOf('list-item')<0 || !found){
-							if(contact.className.lastIndexOf("focused")>-1){
-								found = true;
-							}
-							if(contact.previousSibling == null || contact.previousSibling.nodeName == "H1"){
-								if(contact.parentNode.previousSibling.nodeName!='#text' && contact.parentNode.previousSibling!=null){
-									last = contact.parentNode.previousSibling.querySelectorAll("li").length - 1;
-									contact = contact.parentNode.previousSibling.querySelectorAll("li")[last];
-									decallage = 2*contact.offsetHeight;
-								}
-							}
-							else{
-								contact = contact.previousSibling;
-								decallage = contact.offsetHeight;
-							}
+							rosterPrevious(contact);
 						}
 					}
 					if(contact.offsetTop-document.querySelector('#right').scrollTop < decallage){
 						document.querySelector('#right').scrollTop -= decallage -(contact.offsetTop-document.querySelector('#right').scrollTop) +10;
 					}
+					console.log(contact.offsetTop-document.querySelector('#right').scrollTop+" ? "+decallage);
 					break;
 				//next
 				case e.keyCode = 39:
 				case e.keyCode = 40:
-					decallage = contact.offsetHeight;
-					found = false;
-					if(document.querySelector('.offline').style.display == ''){
-						while((contact.className.lastIndexOf("offline")>-1 || contact.className.lastIndexOf("error")>-1 || !found)){
-							if(contact.className.lastIndexOf("focused") > -1){
-								found = true;
-							}
-							if(contact.nextSibling != null){
-								contact = contact.nextSibling;
-							}
-							else{
-								if(contact.parentNode.nextSibling!=null && contact.parentNode.nextSibling.nodeName != '#text'){
-									contact = contact.parentNode.nextSibling.querySelectorAll("li")[0];
-								}
-								console.log('=/');
-							}
+					if(document.querySelector('.offline').style.display == '' ){
+						while((contact.className.lastIndexOf("offline")>-1 || contact.className.lastIndexOf("error")>-1 || !found) ){
+							rosterNext(contact);
 						}
 					}
 					else{
-						while(contact.getAttribute('style').lastIndexOf('list-item')<0 || !found){
-							if(contact.className.lastIndexOf("focused")>-1){
-								found = true;
-							}
-							if(contact.nextSibling != null){
-								contact = contact.nextSibling;
-							}
-							else{
-								contact = contact.parentNode.nextSibling.querySelectorAll("li")[0];
-							}
+						while((contact.getAttribute('style').lastIndexOf('list-item')<0 || !found) ){
+							rosterNext(contact);
 						}
 					}
-
-					if(contact.offsetTop+decallage-document.querySelector('#right').scrollTop >= document.querySelector('#request').offsetTop){
-						document.querySelector('#right').scrollTop += contact.offsetTop+decallage-document.querySelector('#right').scrollTop - document.querySelector('#request').offsetTop ;
+					if(contact.offsetTop+decallage-document.querySelector('#right').scrollTop >= document.querySelector('#rostermenu').offsetTop){
+						document.querySelector('#right').scrollTop += contact.offsetTop+decallage-document.querySelector('#right').scrollTop - document.querySelector('#rostermenu').offsetTop;
 					}
 					break;
 			}
-			focused.className = focused.className.split(' ')[0];
-			contact.className = contact.className.split(" ")[0] + " focused";
-
-		}*/
+			if(focused.className == (focused.className = focused.className.replace(" focused", "")))
+				focused.className = focused.className.replace("focused", "");
+			if(contact.className.lastIndexOf("focused")<0)
+				contact.className += " focused";
+		}
 	}
 }
