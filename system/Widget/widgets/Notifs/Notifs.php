51e8b5951e8486e6e01fee1fcee8ceb833840a12
Jaussoin Timothée <edhelas@movim.eu>
2012-10-31 20:03:05 +0100

    - Move Notifs widget to the main menu - Complete Notifs widget with pending contact invitation - CSS of Notifs widget - Add realname attribute in RosterLink - Add an empty value in country choice in Vcard widget - Move the logout button - Fix some little bugs in ContactInfo and ContactSummary (empty contact...)

--- system/Widget/widgets/Notifs/Notifs.php
+++ system/Widget/widgets/Notifs/Notifs.php
@@ -24,6 +24,147 @@ class Notifs extends WidgetBase
     {
     	$this->addcss('notifs.css');
     	$this->addjs('notifs.js');
+        $this->registerEvent('notification', 'onNotification');
+    }
+    
+    function ajaxGetNotifications() {
+        $p = new moxl\NotificationGet();
+        $p->setTo($this->user->getLogin())
+          ->request();
+    }
+    
+    function prepareNotifs()
+    {
+        $notifsnum = 0;
+              
+        $html .= '
+            <div id="notifslist">
+                <a 
+                    class="button tiny icon follow" 
+                    href="#" 
+                    id="refresh"
+                    onclick="'.$this->genCallAjax("ajaxGetNotifications").';
+                            this.innerHTML = \''.t('Updating').'\'; 
+                            this.className= \'button tiny icon merged right loading\';
+                            this.onclick = \'return false;\';">
+                    '.t('Rafraichir').'
+                </a>
+                <ul>';
+            // XMPP notifications
+            $notifs = Cache::c('activenotifs');
+            if($notifs == false)
+                $notifs = array();
+            
+            
+            if(sizeof($notifs) != 0) {
+                $notifsnum += sizeof($notifs);
+                
+                $html .= '
+                <li class="title">'.
+                    t('Notifications').'
+                    <span class="num">'.sizeof($notifs).'</span>
+                </li>';
+                
+                foreach($notifs as $n => $val) {
+                    $html .= $val;
+                }
+            
+            }           
+            
+            // Contact request pending
+            $query = RosterLink::query()->where(
+                                        array(
+                                            'key' => $this->user->getLogin(),
+                                            'rosterask' => 'subscribe'));
+            $subscribes = Contact::run_query($query);
+            
+            if(sizeof($subscribes) != 0) {
+                $notifsnum += sizeof($subscribes);
+                
+                $html .= '
+                <li class="title">'.
+                    t('Contact request pending').'
+                    <span class="num">'.sizeof($subscribes).'</span>
+                </li>';
+                
+                foreach($subscribes as $s) {
+                    $html .= '
+                        <li>
+                            <a href="?q=friend&f='.$s->getData('jid').'">
+                            <img class="avatar"  src="'.Contact::getPhotoFromJid('xs', $s->getData('jid')).'" />
+                            '.
+                                $s->getTrueName().'
+                            </a>
+                        </li>';
+                }
+            
+            }
+            
+            
+        $html .= '
+                </ul>
+            </div>';
+            
+        $notifsnew = '';
+        if($notifsnum > 0)
+            $notifsnew = 'class="red"';
+            
+        $html = '
+            <div id="notifstab" onclick="showNotifsList();">
+                <span '.$notifsnew.'>'.
+                    $notifsnum.'
+                </span>
+            </div>'.$html;
+        
+        return $html;
+    }
+    
+    function onNotification($item) {
+        $arr = explodeURI((string)$item->entry->link[0]->attributes()->href);
+        $post = end(explode('/', $arr['node']));
+        //movim_log(explodeURI($item->entry->link[0]->attributes()->href));
+   	    $notifs = Cache::c('activenotifs');
+        
+        $html = '';
+        foreach($notifs as $key => $value)
+            $html .= $this->prepareNotifs($key, $value);
+        
+        $nhtml = '
+        
+            <li>
+                <a href="?q=friend&f='.$arr['path'].'&p='.$post.'">
+                    <span style="font-weight: bold;">'.
+                        (string)$item->entry->source->author->name.'
+                    </span> - '.prepareDate(strtotime((string)$item->entry->published)).'<br />'.
+                    (string)$item->entry->content.'
+                </a>
+            </li>
+                ';
+        
+        $notifs[(string)$item->attributes()->id] = $nhtml;
+   	    
+        RPC::call('movim_fill', 'notifs', RPC::cdata($html));
+        
+	    Cache::c('activenotifs', $notifs);
+    }
+    
+    function build()
+    {
+        ?>
+        <div id="notifs">
+            <?php echo $this->prepareNotifs(); ?>
+        </div>
+        <?php
+    }
+}
+
+/*
+class Notifs extends WidgetBase
+{
+    function WidgetLoad()
+    {
+    	$this->addcss('notifs.css');
+    	$this->addjs('notifs.js');
 
 		$this->registerEvent('message', 'onMessage');
 		$this->registerEvent('subscribe', 'onSubscribe');
@@ -99,37 +240,9 @@ class Notifs extends WidgetBase
         RPC::call('movim_fill', 'notifslist', RPC::cdata($html));
         
 	    Cache::c('activenotifs', $notifs);
-    }
-    
-    function onNotification($item) {
-        $arr = explodeURI((string)$item->entry->link[0]->attributes()->href);
-        $post = end(explode('/', $arr['node']));
-        //movim_log(explodeURI($item->entry->link[0]->attributes()->href));
-   	    $notifs = Cache::c('activenotifs');
-        
-        $html = '';
-        foreach($notifs as $key => $value)
-            $html .= $this->prepareNotifs($key, $value);
-        
-        $nhtml = '
-        <a href="?q=friend&f='.$arr['path'].'&p='.$post.'">
-            <li>
-                
-                    <span style="font-weight: bold;">'.
-                        (string)$item->entry->source->author->name.'
-                    </span> - '.prepareDate(strtotime((string)$item->entry->published)).'<br />'.
-                    (string)$item->entry->content.'
-                
-            </li></a>
-                ';
-        
-        $notifs[(string)$item->attributes()->id] = $nhtml;
-   	    
-        RPC::call('movim_fill', 'notifslist', RPC::cdata($html));
-        
-	    Cache::c('activenotifs', $notifs);
-    }
-    
+    }*/
+
+    /*
     function ajaxSubscribed($jid) {
         $p = new moxl\PresenceSubscribed();
         $p->setTo($jid)
@@ -173,29 +286,8 @@ class Notifs extends WidgetBase
     if($notifs == false)
         $notifs = array();
         
-    /*$query = RosterLink::query()
-                     ->where(
-                        array(
-                            'key' => $this->user->getLogin(),
-                            'jid!' => $this->user->getLogin(),
-                            array(
-                                'rostersubscription!' => 'none',
-                                'rostersubscription!' => 'vcard',
-                                '|rosterask' => 'subscribe')));
-    $contacts = RosterLink::run_query($query);*/
     ?>
     <div id="notifs">
-        <span id="widgettitle">
-            <?php //echo t('Contacts (%s)', sizeof($contacts)); ?>
-            <!--<a 
-                    class="" 
-                    href="#" 
-                    id="addstart"
-                    onclick="addJid(this);">
-                    <?php echo t('Add'); ?>
-            </a>-->
-        </span>
-        
         <!--<a 
             class="button tiny icon yes" 
             href="#" 
@@ -214,4 +306,4 @@ class Notifs extends WidgetBase
     </div>
     <?php    
     }
-}
+}*/
