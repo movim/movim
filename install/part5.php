c8ca09a17c42c18e67c357896c60e547113997cf
Jaussoin Timothée <edhelas@movim.eu>
2012-11-08 11:34:16 +0100

    - Update installer UI with the new CSS - Complete part 5 in the installer - Fix some CSS stuff

--- install/part5.php
+++ install/part5.php
@@ -1,32 +1,41 @@
-<div id="left" style="width: 230px; padding-top: 10px;">
-	<div class="message warning" id="leftside">
-		<p><?php echo t('Move your mousepointer over the configure options to receive more information.');?></p>
-	</div>
-</div>
-<div id="center" style="padding: 20px;" >
-	<h1 style="padding: 0px;"><?php echo $title; ?></h1><br />
-	<?php echo t("You are able to access this installation manager later to make changes to your install. Please specify a username and a password to protect it."); ?>
-    <br /><br />
-    <form method="post" action="index.php">
-        <fieldset>
-            <legend><?php echo $steps[$display]; ?></legend><br />
-                <input type="hidden" name="step" value="5" />
-                <div class="element" <?php echo generate_Tooltip(t("Enter a us")); ?>>
-                    <label for="defBoshURL"><?php echo t("Bosh URL"); ?></label>
-                    <input type="text" id="defBoshURL" name="defBoshURL" value="<? echo get_preset_value('defBoshURL', ''); ?>"/>
+<div id="main">
+    <div id="left">
+        <?php include("error.php"); ?>
+        <div class="message warning" id="leftside">
+            <p><?php echo t('Move your mousepointer over the configure options to receive more information.');?></p>
+        </div>
+    </div>
+    <div id="center">
+        <h1><?php echo $title; ?></h1>
 
-                </div>
-                <div class="element" <?php echo generate_Tooltip(t("Enter here the BOSH-URL in the form: http://domain:123/asd")); ?>>
-                    <label for="defBoshURL"><?php echo t("Bosh URL"); ?></label>
-                    <input type="text" id="defBoshURL" name="defBoshURL" value="<? echo get_preset_value('defBoshURL', ''); ?>"/>
-
-                </div>			                
-        </fieldset>
-        <fieldset id="dbform">
-            
-        </fieldset>
-        <?php include('buttons.php'); ?>
+        <div style="margin: 20px;">
+        <p>
+            <?php echo t("You are able to access this installation manager later to make changes to your install. Please specify a username and a password to protect it."); ?>
+        </p>
         <br />
-    </form>
-</div>
 
+            <form method="post" action="index.php">
+                <fieldset>
+                    <legend><?php echo $steps[$display]; ?></legend><br />
+                        <input type="hidden" name="step" value="5" />
+                        <div class="element" <?php //echo generate_Tooltip(t("Enter a us")); ?>>
+                            <label for="username"><?php echo t("Username"); ?></label>
+                            <input type="text" id="username" name="username" value="<? echo get_preset_value('username', ''); ?>"/>
+
+                        </div>
+                        <div class="element" <?php //echo generate_Tooltip(t("Enter here the BOSH-URL in the form: http://domain:123/asd")); ?>>
+                            <label for="password"><?php echo t("Password"); ?></label>
+                            <input type="password" id="password" name="password" value=""/>
+                        </div>			                
+                        <div class="element" <?php //echo generate_Tooltip(t("Enter here the BOSH-URL in the form: http://domain:123/asd")); ?>>
+                            <label for="repassword"><?php echo t("Retype password"); ?></label>
+                            <input type="password" id="repassword" name="repassword" value=""/>
+                        </div>			                
+                </fieldset>
+
+                <?php include('buttons.php'); ?>
+                <br />
+            </form>
+        </div>
+    </div>
+</div>
