69d7450cae40bcbad4c443c1aeae2941c3754c2c
Jaussoin Timothée <edhelas@movim.eu>
2012-12-13 20:26:45 +0100

    - Fix some little bugs (CSS + post display + add a filter on Feed widget to display only roster contact posts)

--- system/Widget/widgets/Feed/Feed.php
+++ system/Widget/widgets/Feed/Feed.php
@@ -179,21 +179,52 @@ class Feed extends WidgetCommon {
     }
     
     function prepareFeed($start) {
+        $query = RosterLink::query()->where(
+                                        array(
+                                            'RosterLink`.`key' => $this->user->getLogin(),
+                                            array(
+                                                'RosterLink`.`rostersubscription!' => 'none',
+                                                'RosterLink`.`rostersubscription!' => '',
+                                                'RosterLink`.`rostersubscription!' => 'vcard',
+                                                '|RosterLink`.`rosterask' => 'subscribe')))
+                                     ->orderby('RosterLink.group', true);
+
+        $contacts = RosterLink::run_query($query);
+        
+        $rosterc = array();
+        
+        //foreach($contactsq as $c)
+        //    array_push($rosterc, $c->getData('jid'));
+            
+        // We create the array for the comments request
+        $commentid = array();
+        $i = 0;
+            
+        foreach($contacts as $c) {
+            if($i == 0)
+                array_push($rosterc, $c->getData('jid'));
+
+            else
+                array_push($rosterc, '|'.$c->getData('jid'));
+            $i++;
+        }
+        
         // We query the last messages
         $query = Post::query()
                             ->join('Contact', array('Post.jid' => 'Contact.jid'))
                             ->where(
                                 array(
                                     'Post`.`parentid' => '',
-                                    'Post`.`key' => $this->user->getLogin()))
+                                    'Post`.`key' => $this->user->getLogin(),
+                                    array('Post`.`jid' => $rosterc)))
                             ->orderby('Post.updated', true)
-                            ->limit($start, '20');
+                            ->limit($start+1, '10');
         $messages = Post::run_query($query);
 
         // We ask for the HTML of all the posts
         $html = $this->preparePosts($messages);
         
-        $next = $start + 20;
+        $next = $start + 10;
             
         if(sizeof($messages) > 9 && $html != '') {
             $html .= '
@@ -262,7 +293,7 @@ class Feed extends WidgetCommon {
         
         <div id="feedcontent">
         <?php
-            echo $this->prepareFeed(0);
+            echo $this->prepareFeed(-1);
         ?>
         </div>
     </div>
