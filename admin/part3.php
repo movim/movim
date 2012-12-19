8dbf3475190d3b6d938c4b0e40029f36601d31d3
Christine Ho <christine.ho.dev@movim.eu>
2012-12-19 21:23:21 +0100

    - Corrections on the installer (missing "php" in php tags and UTF-8 without BOM encoding of files)

--- admin/part3.php
+++ admin/part3.php
@@ -16,7 +16,7 @@
                         <input type="hidden" name="step" value="3" />
                         <div class="element" <?php //echo generate_Tooltip(t("Enter here the BOSH-URL in the form: http(s)://domain:port/path<br>If you enter an open BOSH-Server, you can connect to many XMPP-Servers. If it is closed, you have to specify the corresponding Server on the next page.<br>If you are unsure about this config option visit the wiki")); ?>>
                             <label for="boshUrl"><?php echo t("Bosh URL"); ?></label>
-                            <input type="text" id="boshUrl" name="boshUrl" value="<? echo get_preset_value('boshUrl', ''); ?> " size="40"/>
+                            <input type="text" id="boshUrl" name="boshUrl" value="<?php echo get_preset_value('boshUrl', ''); ?> " size="40"/>
                         </div>
                         <!--<div class="element" <?php //echo generate_Tooltip(t("When ticked, users can define their own Bosh-Server at the login form.")); ?>>
                                 <label for="userDefinedBosh"><?php echo t('BOSH changeable'); ?></label>
@@ -24,7 +24,7 @@
                         </div>-->
                         <!--<div class="element" <?php //echo generate_Tooltip(t("If you have to connect to the BOSH server with a Proxy fill this form with a proxy URL in the form http://hostname:port otherwise leave blank. <br> If unsure leave blank")); ?>>
                             <label for="boshProxy"><?php echo t("Bosh Proxy"); ?></label>
-                            <input type="text" id="boshProxy" name="boshProxy" value="<? echo get_preset_value('boshProxy', ''); ?> " size="40"/>
+                            <input type="text" id="boshProxy" name="boshProxy" value="<?php echo get_preset_value('boshProxy', ''); ?> " size="40"/>
                         </div>-->
                 </fieldset>
                 <?php include('buttons.php'); ?>
