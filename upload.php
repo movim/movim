b15aaa5eb82162806e9af8e8c45d4cd47fcdda4f
Jaussoin Timothée <edhelas@movim.eu>
2013-03-18 23:18:32 +0100

    - Add index.html in users folders to prevent browsing - Add remove feature in the gallery - Add restrictions in upload.php - Fix some CSS stuff

--- upload.php
+++ upload.php
@@ -17,10 +17,17 @@ define('USER_PATH', BASE_PATH . 'users/'.$user->getLogin().'/');
 
 $error = $_FILES['image_file']['error'];
 
-if ($error == UPLOAD_ERR_OK) {
+if ($error == UPLOAD_ERR_OK && $user->dirSize() < $user->sizelimit) {
     $tmp_name = $_FILES["image_file"]["tmp_name"];
-    $name = stringToUri($_FILES["image_file"]["name"]);
-    move_uploaded_file($tmp_name, USER_PATH.$name);
-    
-    createThumbnailPicture(USER_PATH, $name);
+    if(getimagesize($tmp_name) != 0) {
+        $name = stringToUri($_FILES["image_file"]["name"]);
+        move_uploaded_file($tmp_name, USER_PATH.$name);
+        
+        createThumbnailPicture(USER_PATH, $name);
+    } else {
+        unlink($tmp_name);
+        echo '<div class="message error">'.t('Not a picture').'</div>';
+    }
+} else {
+    echo '<div class="message error">'.t('Folder size limit exceeded').'</div>';
 }
