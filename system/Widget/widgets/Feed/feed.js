84fb872a02c79c010f2833d570c36b40083e55f4
Jaussoin Timothée <edhelas@movim.eu>
2013-04-18 23:59:57 +0200

    - Add SubmitForm in Node widget - Start cleanning old Microblog and Group methods => Pubsub methods

--- system/Widget/widgets/Feed/feed.js
+++ system/Widget/widgets/Feed/feed.js
@@ -19,31 +19,3 @@ function showPosts(n, me) {
         tabs[i].className = '';
     n.className = 'on';
 }
-
-function getFeedMessage() {
-    var text = document.querySelector('#feedmessagecontent');
-    message = text.value;
-    text.value = '';
-    text.blur();
-    return encodeURIComponent(message);
-}
-
-function frameHeight(n) {
-    if(n.className == 'button tiny icon add merged left') {
-        n.className = 'button tiny icon rm merged left';
-    document.querySelector('iframe#feedmessagecontent-frame').style.height = '400px';
-    } else {
-        n.className = 'button tiny icon add merged left';
-        document.querySelector('iframe#feedmessagecontent-frame').style.height = '50px';
-    }
-}
-
-function richText(n) {
-    if(n.className == 'button tiny icon yes merged right') {
-        n.className = 'button tiny icon no merged right';
-        document.querySelector('.menueditor').style.display = 'block';
-    } else {
-        n.className = 'button tiny icon yes merged right';
-        document.querySelector('.menueditor').style.display = 'none';
-    }
-}
