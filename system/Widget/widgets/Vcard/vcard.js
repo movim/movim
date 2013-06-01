5dcf7c1009002895a744050db0708947afc14b95
Jaussoin Timothée <edhelas@movim.eu>
2013-06-01 18:43:02 +0200

    - Fix Webcam snapshot for the avatar for Firefox - CSS integration

--- system/Widget/widgets/Vcard/vcard.js
+++ system/Widget/widgets/Vcard/vcard.js
@@ -72,13 +72,15 @@ function vCardImageLoad(files) {
 
 function showVideo(){
 	navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia; 
-	navigator.getUserMedia({video:true}, successCallback, errorCallback);
+	navigator.getUserMedia({video:true, audio:false}, successCallback, errorCallback);
+    
+    movim_toggle_class('#camdiv', 'active');
 }
 function successCallback(stream) {
   video = document.getElementById("runningcam");
   video.src = window.URL.createObjectURL(stream);
   localMediaStream = stream; // stream available to console
-  video.addEventListener('loadeddata', addCanvasImg, false);
+
   document.getElementById("shoot").addEventListener('click', snapshot, false);
 }
 
@@ -86,23 +88,20 @@ function errorCallback(error){
   console.log("navigator.getUserMedia error: ", error);
 }
 
-function addCanvasImg(){		
-	canvas = document.querySelector("canvas");
-	canvas.width = video.videoWidth;
-	canvas.height = video.videoHeight;
-	
-}
 
 function snapshot() {
 	if (localMediaStream) {
 		canvas = document.querySelector("canvas");
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
 		ctx = canvas.getContext('2d');
 		video = document.getElementById("runningcam");
 		
-		ctx.drawImage(video,0,0);
+		ctx.drawImage(video,0,0, canvas.width, canvas.height);
 		// "image/webp" works in Chrome 18. In other browsers, this will fall back to image/png.
 		var img = new Image();
-            img.src = canvas.toDataURL('image/webp');
+            img.src = canvas.toDataURL('image/png');
+
             img.onload = function() {
                 vCardImageResize(this);
             }
