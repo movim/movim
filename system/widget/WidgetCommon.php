c84e8685fb0cf8ea2df696a2baa193170ae900a7
Jaussoin Timothée <edhelas@movim.eu>
2013-07-06 00:10:11 +0200

    - Remove first paramater -_-

--- system/widget/WidgetCommon.php
+++ system/widget/WidgetCommon.php
@@ -86,7 +86,7 @@ class WidgetCommon extends WidgetBase {
                     <div class="clear"></div>
                 </div>';
 
-        $content = prepareString(html_entity_decode($post->content, ENT_HTML5));
+        $content = prepareString(html_entity_decode($post->content));
         
         if($post->node == 'urn:xmpp:microblog:0')
             $comments = $this->printComments($post, $comments, $public);
