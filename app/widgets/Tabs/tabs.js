bdb341d9af33663216cf6c02da3d27b3f849479a
Christine Ho <nodpounod@gmail.com>
2013-08-11 17:00:58 +0200

    - Add anchor for tabs

--- app/widgets/Tabs/tabs.js
+++ app/widgets/Tabs/tabs.js
@@ -3,17 +3,26 @@ function createTabs() {
     var tabs = document.querySelectorAll('.tabelem');
     
     // We create the list
-    var html =  '<li class="on" onclick="changeTab(this, \'' + tabs[0].id + '\')">' + tabs[0].title +  '</li>';
-    for (var i=1; i<tabs.length; i++){
-        var html = html + '<li onclick="changeTab(this, \'' + tabs[i].id + '\')">' + tabs[i].title +  '</li>';
-        tabs[i].style.display = "none";
+    var html = '';
+	//'<li class="on" onclick="changeTab(this);"><a href="#'+tabs[0].id+'">' + tabs[0].title +  '</a></li>';
+    for (var i=0; i<tabs.length; i++){
+		addclass = '';
+		if(document.URL.search(tabs[i].id)>-1)
+			addclass = 'class="on"';
+        html += '<li ' + addclass + ' onclick="changeTab(this, \''+tabs[i].id+'\');"> <a href="#'+tabs[i].id+'" style="display: block; height:100%; width:100%;">' + tabs[i].title + '</a></li>';
+        //tabs[i].style.display = "none";
     }
+	
     
     // We show the first tab
     tabs[0].style.display = "block";
 
     // We insert the list
     document.querySelector('#navtabs').innerHTML = html;
+	
+	if(document.querySelector(".on") == null){
+		document.querySelector('#navtabs li').click();
+	}
 }
 
 movim_add_onload(function()
@@ -21,7 +30,7 @@ movim_add_onload(function()
     createTabs();
 });
 
-function changeTab(a, n) {
+/*function changeTab(a, n) {
     // We grab the tabs list
     var navtabs = document.querySelector('#navtabs');
     // We clean the class of the li
@@ -33,6 +42,27 @@ function changeTab(a, n) {
     a.className = 'on';
     
     // We hide all the div
+    var tabs = document.querySelectorAll('.tabelem');
+    for (var i=0; i<tabs.length; i++){
+        tabs[i].style.display = "none";
+    }
+    // We show the selected div
+    var tabOn = document.querySelector('#'+n);
+    tabOn.style.display = "block"; 
+}*/
+
+function changeTab(current, n){
+    // We grab the tabs list
+    var navtabs = document.querySelectorAll('#navtabs li');
+    // We clean the class of the li
+    for (var j=0; j<navtabs.length; j++) {
+        navtabs[j].className = '';
+    }
+    
+    // We add the "on" class to the selected li
+    current.className = 'on';
+	
+	// We hide all the div
     var tabs = document.querySelectorAll('.tabelem');
     for (var i=0; i<tabs.length; i++){
         tabs[i].style.display = "none";
