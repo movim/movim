087a4ab04ecdd17169935858f304f9ec1318a862
Jaussoin Timothée <edhelas@movim.eu>
2013-08-12 16:20:30 +0200

    - Clean the old boot system

--- jajax.php
+++ jajax.php
@@ -15,24 +15,6 @@
  * 
  * See the file `COPYING' for licensing information.
  */
-/**
-* BOOTSTRAP
-**/
-/*define('DOCUMENT_ROOT',  dirname(__FILE__));
-require_once(DOCUMENT_ROOT.'/system/Utils.php');
-require_once(DOCUMENT_ROOT.'/system/Conf.php');
-try {
-    define('ENVIRONMENT',Conf::getServerConfElement('environment'));
-} catch (Exception $e) {
-    define('ENVIRONMENT','production');//default environment is production
-}
-ini_set('log_errors', 1);
-ini_set('display_errors', 0);
-ini_set('error_reporting', E_ALL ^ E_DEPRECATED ^ E_NOTICE);
-ini_set('error_log', DOCUMENT_ROOT.'/log/php.log');*/
-
-// Run
-//require_once('init.php');
 
 define('DOCUMENT_ROOT',  dirname(__FILE__));
 require_once(DOCUMENT_ROOT.'/bootstrap.php');
