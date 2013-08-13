b2933eee967f72f57ad6b548addf2624626ddc5e
Jaussoin Timothée <edhelas@movim.eu>
2013-08-13 23:41:33 +0200

    - Add a "Discover" page for the public profiles - Little CSS fixes - Little Route fixes

--- app/widgets/Blog/Blog.php
+++ app/widgets/Blog/Blog.php
@@ -7,7 +7,8 @@ class Blog extends WidgetCommon {
         $node = $_GET['n'];
         
         $this->view->assign('from', $from);
-        $this->view->assign('node', $node);
+        if(isset($node))
+            $this->view->assign('node', $node);
         
         $pd = new \modl\PostnDAO();
         $messages = $pd->getPublic($from, $node);
@@ -20,9 +21,9 @@ class Blog extends WidgetCommon {
                 $n = $pd->getNode($from, $node);
                 if(isset($n->title))
                     $this->view->assign('title', $n->title);
-                else
+                elseif(isset($n->nodeid))
                     $this->view->assign('title', $n->nodeid);
-            // Fir a simple contact
+            // For a simple contact
             } else {
                 $this->view->assign('title', t("%s's feed",$messages[0]->getContact()->getTrueName()));
                 $this->view->assign('logo', $messages[0]->getContact()->getPhoto('l'));
@@ -31,6 +32,8 @@ class Blog extends WidgetCommon {
             $this->view->assign('date', date('c'));
             $this->view->assign('name', $messages[0]->getContact()->getTrueName());
             $this->view->assign('feed', Route::urlize('feed',array($from, $node)));
+        } else {
+            $this->view->assign('title', t('Feed'));
         }
         
         $this->view->assign('posts', $this->preparePosts($messages, true));
