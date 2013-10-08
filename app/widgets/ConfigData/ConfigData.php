46f57f5c558dc62776a5ef187113ac84bae29a5d
Jaussoin Timothée <edhelas@movim.eu>
2013-10-08 22:03:34 +0200

    - Clean old commented code - Fix a little issue in Vcard submition

--- app/widgets/ConfigData/ConfigData.php
+++ app/widgets/ConfigData/ConfigData.php
@@ -71,60 +71,7 @@ class ConfigData extends WidgetBase
     }
     
     function refresh() {
-        //$html = $this->prepareConfigData();
-        //RPC::call('movim_fill', 'configdata', $html);
         RPC::call('movim_reload_this');
         RPC::commit();
     }
-    
-    /*function prepareConfigData() {
-        $cd = new \modl\ContactDAO();
-        $stats = $cd->getStatistics();
-
-        $clearrosterlink    = $this->genCallAjax('ajaxClearRosterLink');
-        $clearmessage       = $this->genCallAjax('ajaxClearMessage');
-        $clearpost          = $this->genCallAjax('ajaxClearPost');
-        
-        $html = '
-            <form enctype="multipart/form-data" method="post" action="index.php" name="general">
-                <fieldset>
-                    <legend>'.t('Cache').'</legend>
-                    <div class="clear"></div>
-                    <div class="element thin">
-                        <label for="name">'.t('Post'). ' - '.$stats['post'].'</label><br />
-                        <a 
-                            type="button" 
-                            name="email" 
-                            class="button icon color red back"
-                            onclick="'.$clearpost.'">'.t('Clear').'</a>
-                    </div>
-                    <div class="element thin">
-                        <label for="name">'.t('Messages'). ' - '.$stats['message'].'</label><br />
-                        <a 
-                            type="button" 
-                            name="email" 
-                            class="button icon color red back"
-                            onclick="'.$clearmessage.'">'.t('Clear').'</a>
-                    </div>
-                    <div class="element thin">
-                        <label for="name">'.t('Contacts'). ' - '.$stats['rosterlink'].'</label><br />
-                        <a 
-                            type="button" 
-                            class="button icon color red back"
-                            onclick="'.$clearrosterlink.'">'.t('Clear').'</a>
-                    </div>
-                </fieldset>
-            </form>';
-        return $html;
-    }*/
-    
-    /*function build()
-    {
-        ?>
-        <div class="tabelem padded" title="<?php echo t('Data'); ?>" id="configdata" >
-            <?php echo $this->prepareConfigData(); ?>
-        </div>
-        <?php
-    }
-    */
 }
