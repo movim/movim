1b16f0ae577c8902b992e541c75fbaee2b9de37b
Jaussoin Timothée <edhelas@movim.eu>
2013-11-01 21:23:42 +0100

    - Change the size of the session column to prevent key creation error on old MySQL servers

--- app/models/session/Session.php
+++ app/models/session/Session.php
@@ -13,13 +13,13 @@ class Session extends ModlModel {
         $this->_struct = '
         {
             "name" : 
-                {"type":"string", "size":128, "mandatory":true, "key":true },
+                {"type":"string", "size":32, "mandatory":true, "key":true },
             "value" : 
                 {"type":"text", "mandatory":true },
             "session" : 
                 {"type":"string", "size":128, "mandatory":true, "key":true },
             "container" : 
-                {"type":"string", "size":128, "mandatory":true, "key":true },
+                {"type":"string", "size":16, "mandatory":true, "key":true },
             "timestamp" : 
                 {"type":"date" }
         }';
