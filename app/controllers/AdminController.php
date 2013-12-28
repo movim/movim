3e4509b76c83023ea4331660b63060ed3d19ba79
Jaussoin Timothée <edhelas@movim.eu>
2013-12-28 14:49:01 +0100

    - Rewrite de administration login system to a simple PHP session one

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -6,29 +6,15 @@ class AdminController extends BaseController {
     }
 
     function dispatch() {
-        if(!isset($_SERVER['PHP_AUTH_USER'])) {
-            $this->authenticate();
+        session_start();
+        
+        if(isset($_SESSION['admin']) && $_SESSION['admin'] == true) {            
+            $this->page->setTitle(t('%s - Administration Panel', APP_TITLE));
+
+            $this->page->menuAddLink(t('Home'), 'main');
+            $this->page->menuAddLink(t('Administration'), 'admin', true);
         } else {
-            $conf = \system\Conf::getServerConf();
-
-            if($_SERVER['PHP_AUTH_USER'] == (string)$conf['user'] && sha1($_SERVER['PHP_AUTH_PW']) == (string)$conf['pass']){
-                $this->page->setTitle(t('%s - Administration Panel', APP_TITLE));
-
-                $this->page->menuAddLink(t('Home'), 'main');
-                $this->page->menuAddLink(t('Administration'), 'admin', true);
-            } else
-                $this->authenticate();
+            $this->name = 'adminlogin';
         }
     }
-
-    /**
-     * Return a basic auth page for the administration area
-     */
-    protected function authenticate(){
-        header('WWW-Authenticate: Basic realm="Enter credentials admin/password"');
-        header('HTTP/1.0 401 Unauthorized');
-        echo 'Why are you hitting cancel?';
-        exit;
-    }
-
 }
