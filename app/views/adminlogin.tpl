e5a314136632bddf9906724950b0b9198d29a9d8
Jaussoin Timothée <edhelas@movim.eu>
2013-12-28 14:49:01 +0100

    - Rewrite de administration login system to a simple PHP session one

---
+++ app/views/adminlogin.tpl
@@ -0,0 +1,10 @@
+<div id="main">
+    <div id="center" >
+		<div class="fixed_block">
+			<h1><?php echo t('Login'); ?></h1>
+		</div>
+		<div class="moving_block" >
+			<?php $this->widget('AdminLogin');?>
+		</div>
+    </div>
+</div>
---
+++ app/widgets/AdminLogin/AdminLogin.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @package Widgets
+ *
+ * @file AdminLogin.php
+ * This file is part of MOVIM.
+ *
+ * @brief The login administration widget.
+ *
+ * @author Timothée Jaussoin <edhelas@gmail.com>
+ *
+ * Copyright (C)2013 MOVIM project
+ *
+ * See COPYING for licensing information.
+ */
+ 
+class AdminLogin extends WidgetBase {  
+    function WidgetLoad()
+    {
+
+
+    }
+    
+}
