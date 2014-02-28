6e71db33464455aec8ce5f7cf99ce58552b8678b
Jaussoin Timothée <edhelas@movim.eu>
2014-02-28 19:31:18 +0100

    - Add a new "Conference" model - Rewrite the Bookmark widget - Rewrite the whole MUC system - Split some templates - Fix littles warnings

---
+++ app/models/conference/Conference.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace modl;
+
+class Conference extends ModlModel {    
+    public $jid;
+    public $conference;
+    public $name;
+    public $nick;
+    public $autojoin;
+    public $status;
+    
+    public function __construct() {
+        $this->_struct = '
+        {
+            "jid" : 
+                {"type":"string", "size":128, "mandatory":true, "key":true },
+            "conference" : 
+                {"type":"string", "size":128, "mandatory":true, "key":true },
+            "name" : 
+                {"type":"string", "size":128, "mandatory":true },
+            "nick" : 
+                {"type":"string", "size":128 },
+            "autojoin" : 
+                {"type":"int", "size":1 },
+            "status" : 
+                {"type":"int", "size":1 }
+        }';
+        
+        parent::__construct();
+    }
+}
---
+++ app/widgets/Chat/_chat_muc_list.tpl
@@ -0,0 +1,9 @@
+<ul>
+{loop="muclist"}
+    {if="$value->value < 5"}
+        <li class="{$c->colorNameMuc($value->ressource)}">
+            {$value->ressource}
+        </li>
+    {/if}
+{/loop}
+</ul>
