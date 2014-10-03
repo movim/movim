923d38ac948f337ea64b68dcabe1ff01fed64b32
Jaussoin Timothée <edhelas@movim.eu>
2014-10-03 23:51:36 +0200

    - Fix the function handling in Movim RPC - Handle Caps and Disco requests - Comment the Disco requests for the moment for performance issues

--- app/widgets/Ack/Ack.php
+++ app/widgets/Ack/Ack.php
@@ -18,15 +18,30 @@
  */
 
 use Moxl\Xec\Action\Ack\Send;
+use Moxl\Xec\Action\Disco\Request;
+use Moxl\Stanza\Disco;
  
 class Ack extends WidgetBase {
     function load()
     {
         $this->registerEvent('ack', 'onAckRequest');
-        
+        $this->registerEvent('discoinfo', 'onDiscoInfoRequest');
+        $this->registerEvent('caps', 'onCapsRequest');
+    }
+
+    function display()
+    {
         $this->view->assign('ack', 
                             $this->genCallAjax(
                                 "ajaxAckRequest", 'to', 'id')
+                        );
+        $this->view->assign('discoinfo', 
+                            $this->genCallAjax(
+                                "ajaxDiscoInfoRequest", 'to', 'id')
+                        );
+        $this->view->assign('caps', 
+                            $this->genCallAjax(
+                                "ajaxCapsRequest", 'to', 'node')
                         );
     }
     
@@ -36,10 +51,33 @@ class Ack extends WidgetBase {
         RPC::call('ackRequest', $to, $id);
     }
     
+    function onDiscoInfoRequest($packet) {
+        $to = $packet->content[0];
+        $id = $packet->content[1];
+        RPC::call('discoInfoRequest', $to, $id);
+    }
+    
+    function onCapsRequest($packet) {
+        $to     = $packet->content[0];
+        $node   = $packet->content[1];
+        RPC::call('capsRequest', $to, $node);
+    }
+    
     function ajaxAckRequest($to, $id) {       
         $ack = new Send;
         $ack->setTo($to)
             ->setId($id)
             ->request();
+    }
+    
+    function ajaxDiscoInfoRequest($to, $id) {       
+        //Disco::answer($jid, $id);
+    }
+    
+    function ajaxCapsRequest($to, $node) {       
+        $d = new Request;
+        $d->setTo($to)
+          ->setNode($node)
+          ->request();
     }
 }
