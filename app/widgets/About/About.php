94752355527d696a9412c1f647ccb0f9e925870f
Jaussoin Timothée <edhelas@movim.eu>
2014-10-15 22:55:56 +0200

    - Add a new widget which display a nice looking version of the XEP - Add an another US flag - Fix the post CSS

--- app/widgets/About/About.php
+++ app/widgets/About/About.php
@@ -3,17 +3,12 @@
 /**
  * @package Widgets
  *
- * @file Vcard.php
+ * @file About.php
  * This file is part of MOVIM.
  * 
  * @brief A widget which display some help 
  *
- * @author Timothée    Jaussoin <edhelas_at_gmail_dot_com>
- *
- * @version 1.0
- * @date 3 may 2012
- *
- * Copyright (C)2010 MOVIM project
+ * @author Timothée
  * 
  * See COPYING for licensing information.
  */
