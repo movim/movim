8766f258fafe05c1285db1725d203d04de271e82
Jaussoin Timothée <edhelas@movim.eu>
2014-10-28 00:17:19 +0100

    - Global XMPP session management

---
+++ src/Moxl/Stanza/Stream.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Moxl\Stanza;
+
+class Stream {
+    static function init($to)
+    {
+        $xml = '<stream:stream xmlns:stream="http://etherx.jabber.org/streams" version="1.0" xmlns="jabber:client" to="'.$to.'" >';
+        \Moxl\API::request($xml);
+    }
+
+    static function end()
+    {
+        $xml = '</stream:stream>';
+        \Moxl\API::request($xml);
+    }
+
+    static function bindSet($ressource)
+    {
+        $xml = '
+            <bind xmlns="urn:ietf:params:xml:ns:xmpp-bind">
+                <resource>'.$ressource.'</resource>
+            </bind>';
+        $xml = \Moxl\API::iqWrapper($xml, false, 'set');
+        \Moxl\API::request($xml);
+    }
+    
+    static function sessionStart($to)
+    {
+        $xml = '
+            <session xmlns="urn:ietf:params:xml:ns:xmpp-session"/>';
+        $xml = \Moxl\API::iqWrapper($xml, $to, 'set');
+        \Moxl\API::request($xml);
+    }
+}
---
+++ src/Moxl/Xec/Action/Session/Bind.php
@@ -0,0 +1,58 @@
+<?php
+/*
+ * Bind.php
+ * 
+ * Copyright 2012 edhelas <edhelas@edhelas-laptop>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+namespace Moxl\Xec\Action\Session;
+
+use Moxl\Xec\Action;
+use Moxl\Stanza\Stream;
+use Moxl\Utils;
+
+class Bind extends Action
+{
+    private $_to;
+    
+    public function request() 
+    {
+        $this->store();
+        Stream::bindSet($this->_ressource);
+    }
+    
+    public function setRessource($ressource)
+    {
+        $this->_ressource = $ressource;
+        return $this;
+    }
+    
+    public function handle($stanza, $parent = false) {
+        //$session = \Sessionx::start();
+        list($jid, $ressource) = explode('/', (string)$stanza->jid);
+        list($session->username, $session->host) = explode('@',$jid);
+        if($ressource)
+            $session->ressource = $ressource;
+
+        $ss = new Start;
+        $ss->setTo($session->host)
+           ->request();
+    }
+}
