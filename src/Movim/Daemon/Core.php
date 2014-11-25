4dd6a0cc4699eff1bfa137a05d0dc89a00e1f140
Jaussoin Timothée <edhelas@movim.eu>
2014-11-25 23:47:36 +0100

    - Add a session cleaner which close properly the linkers after a while - Remove manual session cleaning in the login - Fix a little issue in the session process affectation when the linker is killed

--- src/Movim/Daemon/Core.php
+++ src/Movim/Daemon/Core.php
@@ -10,6 +10,8 @@ class Core implements MessageComponentInterface {
     public $loop;
     public $baseuri;
 
+    private $cleanerdelay = 3; // in minutes
+
     public function __construct($loop, $baseuri, $port)
     {
         echo colorize("Movim daemon launched\n", 'green');
@@ -18,6 +20,8 @@ class Core implements MessageComponentInterface {
         
         $this->loop    = $loop;
         $this->baseuri = $baseuri;
+
+        $this->registerCleaner();
     }
     
     public function onOpen(ConnectionInterface $conn)
@@ -45,14 +49,34 @@ class Core implements MessageComponentInterface {
         $sid = $this->getSid($conn);
         if($sid != null) {
             $this->sessions[$sid]->detach($conn);
-
-            // No WebSockets and no linker ? We close the whole session
-            if($this->sessions[$sid]->countClients() == 0
-            && $this->sessions[$sid]->process == null) {
-                unset($this->sessions[$sid]);
-            }
+            $this->closeEmptySession($sid);
         }
     }
+
+    private function registerCleaner()
+    {
+        $this->loop->addPeriodicTimer(5, function() {
+            foreach($this->sessions as $sid => $session) {
+                if(time()-$session->timestamp > $this->cleanerdelay*60) {
+                    $session->killLinker();
+                    $this->closeEmptySession($sid);
+                }
+            }
+        });
+    }
+
+    private function closeEmptySession($sid)
+    {
+        // No WebSockets and no linker ? We close the whole session
+        if($this->sessions[$sid]->countClients() == 0
+        && $this->sessions[$sid]->process == null) {
+            $sd = new \Modl\SessionxDAO();
+            $sd->delete($sid);
+            
+            unset($this->sessions[$sid]);
+        }
+    }
+    
     public function onError(ConnectionInterface $conn, \Exception $e)
     {
         echo "An error has occurred: {$e->getMessage()}\n";
