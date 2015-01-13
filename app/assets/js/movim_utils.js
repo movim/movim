70537dcc76571a42135441b903a4f70c05eb79d8
Christine Ho <nodpounod@gmail.com>
2015-01-13 11:25:07 +0100

    - Sanitize strings for easy search

--- app/assets/js/movim_utils.js
+++ app/assets/js/movim_utils.js
@@ -282,3 +282,25 @@ function base64_decode(data) {
 
     return dec.replace(/\0+$/, '');
 }
+
+/**
+ * @brief Sanitize string for easy search
+ * @param string
+ */
+function accentsTidy(s){
+    //Ian Elliott in http://stackoverflow.com/questions/990904/javascript-remove-accents-diacritics-in-strings
+    var r = s.toLowerCase();
+    r = r.replace(new RegExp("\\s", 'g'),"");
+    r = r.replace(new RegExp("[àáâãäå]", 'g'),"a");
+    r = r.replace(new RegExp("æ", 'g'),"ae");
+    r = r.replace(new RegExp("ç", 'g'),"c");
+    r = r.replace(new RegExp("[èéêë]", 'g'),"e");
+    r = r.replace(new RegExp("[ìíîï]", 'g'),"i");
+    r = r.replace(new RegExp("ñ", 'g'),"n");                            
+    r = r.replace(new RegExp("[òóôõö]", 'g'),"o");
+    r = r.replace(new RegExp("œ", 'g'),"oe");
+    r = r.replace(new RegExp("[ùúûü]", 'g'),"u");
+    r = r.replace(new RegExp("[ýÿ]", 'g'),"y");
+    r = r.replace(new RegExp("\\W", 'g'),"");
+    return r;
+};
