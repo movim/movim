14cfe9f314ebc4152fba2c7a6f4a35550e663dc9
Jaussoin Timothée <edhelas@movim.eu>
2015-01-19 14:00:21 +0100

    - Echap some character in the Core.php file - Continue Material design integration in the Admin panel

--- src/Movim/Daemon/Core.php
+++ src/Movim/Daemon/Core.php
@@ -49,11 +49,11 @@ class Core implements MessageComponentInterface {
         echo "location /ws/ {
     proxy_pass http://{$explode['host']}:{$port}/;
     proxy_http_version 1.1;
-    proxy_set_header Upgrade $http_upgrade;
+    proxy_set_header Upgrade \$http_upgrade;
     proxy_set_header Connection \"Upgrade\";
-    proxy_set_header Host $host;
-    proxy_set_header X-Real-IP $remote_addr;
-    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    proxy_set_header Host \$host;
+    proxy_set_header X-Real-IP \$remote_addr;
+    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
     proxy_set_header X-Forwarded-Proto https;
     proxy_read_timeout 86400s;
     proxy_send_timeout 86400s;
