722ad2dc0e58eef96364b2db597f046943ec32be
Jaussoin Timothée <edhelas@movim.eu>
2015-01-26 18:21:01 +0100

    - Fix various CSS issues - Use flexbox - Fix notification desktop display

--- app/widgets/Chats/chats.js
+++ app/widgets/Chats/chats.js
@@ -15,7 +15,7 @@ var Chats = {
                     Chat_ajaxGet(this.dataset.jid);
                     Chats.reset(items);
                     Notification_ajaxClear('chat|' + this.dataset.jid);
-                    Notification_ajaxCurrent('chat|' + this.dataset.jid);
+                    Notification.current('chat|' + this.dataset.jid);
                     movim_add_class(this, 'active');
                 }
 
