c55bbe723e3c17cf21226a47e3648d6fdfcdf51b
Jaussoin Timothée <edhelas@movim.eu>
2015-03-16 23:52:13 +0100

    - Complete the XEP-0084 Avatar implementation with the support of urn:xmpp:avatar:metadata - Add a new access_model in Pubsub and PEP nodes configuration - Clean some code

---
+++ src/Moxl/Xec/Payload/AvatarMetadata.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Moxl\Xec\Payload;
+
+use Moxl\Xec\Action\Avatar\Get;
+
+class AvatarMetadata extends Payload
+{
+    public function handle($stanza, $parent = false) {        
+        $jid = current(explode('/',(string)$parent->attributes()->from));
+
+        $evt = new \Event();
+            
+        $cd = new \modl\ContactDAO();
+        $c = $cd->get($jid);
+
+        if(isset($stanza->items->item->metadata->info)) {
+            $info = $stanza->items->item->metadata->info->attributes();
+
+            if($info->id != $c->avatarhash) {
+                $c->avatarhash = $info->id;
+                $cd->set($c);
+                
+                $g = new Get;
+                $g->setTo($jid)
+                  ->request();
+            }
+        }
+    }
+}
