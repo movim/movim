06bb759fb4d501cee0b09f0a1a7e6c2ea473363b
Jaussoin Timothée <edhelas@movim.eu>
2015-04-04 20:14:44 +0200

    - Add the Groups page

---
+++ app/controllers/GroupController.php
@@ -0,0 +1,11 @@
+<?php
+
+class GroupController extends BaseController {
+    function load() {
+        $this->session_only = true;
+    }
+
+    function dispatch() {
+        $this->page->setTitle(__('title.main', APP_TITLE));
+    }
+}
