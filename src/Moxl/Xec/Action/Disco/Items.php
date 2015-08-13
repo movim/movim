20518e361bd48230d62180a733b82da2c782301c
Jaussoin Timothée <edhelas@movim.eu>
2015-08-13 21:57:41 +0200

    - Move Pubsub DiscoItems to Disco Items

---
+++ src/Moxl/Xec/Action/Disco/Items.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Moxl\Xec\Action\Disco;
+
+use Moxl\Xec\Action;
+use Moxl\Xec\Action\Disco\Request;
+use Moxl\Stanza\Disco;
+
+class Items extends Action
+{
+    private $_to;
+
+    public function request()
+    {
+        $this->store();
+        Disco::items($this->_to);
+    }
+
+    public function setTo($to)
+    {
+        $this->_to = echapJid($to);
+        return $this;
+    }
+
+    public function handle($stanza, $parent = false) {
+        $nd = new \Modl\ItemDAO();
+
+        foreach($stanza->query->item as $item) {
+            $n = new \Modl\Item();
+            $n->set($item, $this->_to);
+            if(substr($n->node, 0, 29) != 'urn:xmpp:microblog:0:comments')
+                $nd->set($n, true);
+
+            $r = new Request;
+            $r->setTo($n->jid)
+              ->request();
+        }
+
+        $this->pack($this->_to);
+        $this->deliver();
+    }
+
+    public function error($error) {
+        $this->pack($this->_to);
+        $this->deliver();
+    }
+}
