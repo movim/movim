ed932bdfd4b40a37c29d1d9374fd1ae21847c5be
Jaussoin Timothée <edhelas@movim.eu>
2015-10-18 13:03:32 +0200

    Rewrite the all lib from string concatenation to full DomDocument generation

--- lib/XMPPtoForm.php
+++ lib/XMPPtoForm.php
@@ -6,14 +6,14 @@ class XMPPtoForm{
 
     public function __construct(){
         $this->fieldset = 0;
-        $this->html = '';
+        $this->html = new \DOMDocument('1.0', 'UTF-8');
         $this->xmpp = '';
     }
 
     public function getHTML($xmpp){
         $this->setXMPP($xmpp);
         $this->create();
-        return $this->html;
+        return $this->html->saveXML();
     }
 
     public function setXMPP($xmpp){
@@ -25,7 +25,6 @@ class XMPPtoForm{
         $x = new SimpleXMLElement($this->xmpp);
 
         foreach($x->children() as $element){
-
             switch($element->getName()){
                 case "title":
                     $this->outTitle($element);
@@ -34,15 +33,15 @@ class XMPPtoForm{
                     $this->outP($element);
                     break;
                 case "field":
-                    if($element['type'] != 'hidden' && $element['type'] != 'fixed')
-                        $this->html .='<div>';
+                    //if($element['type'] != 'hidden' && $element['type'] != 'fixed')
+                    //    $this->html .='<div>';
                     switch($element['type']){
                         case "boolean":
                             $this->outCheckbox($element);
                             break;
-                        case "fixed":
-                            $this->outBold($element);
-                            break;
+                        //case "fixed":
+                        //    $this->outBold($element);
+                        //    break;
                         case "text-single":
                             $this->outInput($element, "", "");
                             break;
@@ -68,10 +67,10 @@ class XMPPtoForm{
                             $this->outInput($element, "email", "");
                             break;
                         default:
-                            $this->html .= "";
+                            //$this->html .= "";
                     }
-                    if($element['type'] != 'hidden')
-                        $this->html .='</div>';
+                    //if($element['type'] != 'hidden')
+                    //    $this->html .='</div>';
                     break;
                 case 'url':
 
@@ -80,50 +79,64 @@ class XMPPtoForm{
                 case 'username':
                 case 'email':
                 case 'password':
-                    $this->html .='<div class="element">';
+                    //$this->html .='<div class="element">';
                         $this->outGeneric($element->getName());
-                    $this->html .='</div>';
+                    //$this->html .='</div>';
                     break;
                 default:
-                    $this->html .= "";
+                    //$this->html .= "";
             }
         }
-        if($this->fieldset>0){
+        /*if($this->fieldset>0){
             $this->html .= '</fieldset>';
-        }
+        }*/
     }
 
     private function outGeneric($s){
-        $this->html .= '
-            <input id="'.$s.'" name="generic_'.$s.'" type="'.$s.'" required/>
-            <label for="'.$s.'">'.
-                $s.'
-            </label>
-            ';
+        $div = $this->html->createElement('div');
+        $div->setAttribute('class', 'element');
+        $this->html->appendChild($div);
+
+        $input = $this->html->createElement('input');
+        $input->setAttribute('type', $s);
+        $input->setAttribute('id', $s);
+        $input->setAttribute('name', 'generic_'.$s);
+        $input->setAttribute('required', 'required');
+        
+        $div->appendChild($input);
+
+        $label = $this->html->createElement('label', $s);
+        $label->setAttribute('for', $s);
+        $div->appendChild($label);
     }
     private function outTitle($s){
-        $this->html .= '
-            <ul class="thin simple">
-                <li>
-                    <span>'.$s.'</span>
-                </li>
-            </ul>';
+        $ul = $this->html->createElement('ul');
+        $ul->setAttribute('class', 'thin simple');
+        $this->html->appendChild($ul);
+
+        $li = $this->html->createElement('li');
+        $li->appendChild($this->html->createElement('span', $s));
+
+        $ul->appendChild($li);
     }
 
     private function outP($s){
-        $this->html .= '
-            <ul class="thin simple">
-                <li>
-                    <p>'.$s.'</p>
-                </li>
-            </ul>';
+        $ul = $this->html->createElement('ul');
+        $ul->setAttribute('class', 'thin simple');
+        $this->html->appendChild($ul);
+
+        $li = $this->html->createElement('li');
+        $ul->appendChild($li);
+
+        $li->appendChild($this->html->createElement('p', $s));
     }
 
     private function outUrl($s) {
-        $this->html .= '
-            <a href="'.$s->getName().'">'.$s->getName().'</a>';
+        $a = $this->html->createElement('a', $s->getName());
+        $a->setAttribute('href', $s->getName());
+        $this->html->appendChild($a);
     }
-
+    /*
     private function outBold($s){
         if($this->fieldset > 0){
             $this->html .= '</fieldset>';
@@ -131,206 +144,208 @@ class XMPPtoForm{
         $this->html .= '<fieldset><legend>'.$s->value.'</legend><br />';
         $this->fieldset ++;
     }
-
+    */
     private function outCheckbox($s){
-        $this->html .= '
-            <div class="select">
-                <select
-                    type="'.$s['type'].'"
-                    label="'.$s['label'].'"
-                    id="'.$s["var"].'"
-                    name="'.$s['var'].'" '.$multiple.' '.$s->required.'>';
+        $container = $this->html->createElement('div');
+        $this->html->appendChild($container);
 
-        $this->html .= '
-            <option value="true"';
-            if(isset($s->value) || $s->value == "true" || $s->value == "1")
-                $this->html .= ' selected="selected"';
-        $this->html .= '>'.__('button.bool_yes').'</option>';
+        $div = $this->html->createElement('div');
+        $div->setAttribute('class', 'select');
+        $container->appendChild($div);
 
-        $this->html .= '
-            <option value="false"';
-            if(!isset($s->value) || $s->value == "false" || $s->value == "0")
-                $this->html .= ' selected="selected"';
-        $this->html .= '>'.__('button.bool_no').'</option>';
+        $select = $this->html->createElement('select');
+        $select->setAttribute('type', $s['type']);
+        $select->setAttribute('label', $s['label']);
+        $select->setAttribute('id', $s['var']);
+        $select->setAttribute('name', $s['var']);
 
-        $this->html .= '
-                </select>
-            </div>
-            <label for="'.$s["var"].'">'.$s["label"].'</label>';
+        if($s->required)
+            $select->setAttribute('required', 'required');
+        
+        $div->appendChild($select);
+
+        $option = $this->html->createElement('option', __('button.bool_yes'));
+        $option->setAttribute('value', 'true');
+        if(isset($s->value) || $s->value == "true" || $s->value == "1")
+            $option->setAttribute('selected', 'selected');
+        $select->appendChild($option);
+
+        $option = $this->html->createElement('option', __('button.bool_no'));
+        $option->setAttribute('value', 'false');
+        if(!isset($s->value) || $s->value == "false" || $s->value == "0")
+            $option->setAttribute('selected', 'selected');
+        $select->appendChild($option);
+
+        $label = $this->html->createElement('label', $s['label']);
+        $label->setAttribute('for', $s['var']);
+        $container->appendChild($label);
     }
 
     private function outTextarea($s){
-        $this->html .= '
-            <textarea
-                id="'.$s["var"].'"
-                name="'.$s["var"].'"
-                type="'.$s['type'].'"
-                label="'.$s['label'].'"
-                required="'.$s->required.'">';
+        $container = $this->html->createElement('div');
+        $this->html->appendChild($container);
+
+        $textarea = $this->html->createElement('textarea');
+        $textarea->setAttribute('type', $s['type']);
+        $textarea->setAttribute('label', $s['label']);
+        $textarea->setAttribute('id', $s['var']);
+        $textarea->setAttribute('name', $s['var']);
+
+        if($s->required)
+            $textarea->setAttribute('required', 'required');
 
         foreach($s->children() as $value){
             if($value->getName() == "value"){
-                $this->html .= $value. "\n";
+                $textarea->nodeValue .= $value . "\n";
             }
         }
-        $this->html .= '</textarea>';
+        
+        $container->appendChild($textarea);
 
-        $this->html .= '
-            <label for="'.$s["var"].'">'.$s["label"].'</label>';
+        $label = $this->html->createElement('label', $s['label']);
+        $label->setAttribute('for', $s['var']);
+        $container->appendChild($label);
     }
 
     private function outInput($s, $type, $multiple){
+        $container = $this->html->createElement('div');
+        $this->html->appendChild($container);
+        
+        $input = $this->html->createElement('input');
+        $input->setAttribute('id', $s['var']);
+        $input->setAttribute('name', $s['var']);
+        $input->setAttribute('type', $type);
+        $input->setAttribute('title', $s->desc);
+        $input->setAttribute('type', $s['type']);
+        $input->setAttribute('label', $s['label']);
+
         if($s->required)
-            $req = 'required';
-        $this->html .= '
-            <input id="'.$s["var"].'" name="'.$s["var"].'" value="';
+            $input->setAttribute('required', 'required');
 
-            foreach($s->children() as $value){
-                if($value->getName() == "value"){
-                    $this->html .= $value;
-                }
+        foreach($s->children() as $value){
+            if($value->getName() == "value"){
+                $input->setAttribute('value', $value);
             }
-
-        $this->html .= '"';
+        }
 
         if($s['var'] == 'username')
-            $this->html .= ' pattern="[a-z0-9_-]*" ';
+            $input->setAttribute('pattern', '[a-z0-9_-]*');
+        
+        $container->appendChild($input);
 
-        $this->html .= '
-            type="'.$type.'"
-            title="'.$s->desc.'"
-            type="'.$s['type'].'"
-            label="'.$s['label'].'"
-            '.$multiple.' '.$req.'/>
-            <label for="'.$s["var"].'">'.$s["label"].'</label>';
+        $label = $this->html->createElement('label', $s['label']);
+        $label->setAttribute('for', $s['var']);
+        $container->appendChild($label);
     }
 
     private function outHiddeninput($s){
-        $this->html .= '
-            <input type="hidden" name="'.$s["var"].'" value="'.$s->value.'" />';
+        $input = $this->html->createElement('input');
+        $input->setAttribute('name', $s['var']);
+        $input->setAttribute('type', 'hidden');
+        $input->setAttribute('value', $s->value);
+
+        $this->html->appendChild($input);
     }
 
     private function outList($s, $multiple){
-        $this->html .= '
-            <div class="select">
-                <select
-                    type="'.$s['type'].'"
-                    label="'.$s['label'].'"
-                    id="'.$s["var"].'"
-                    name="'.$s['var'].'" '.$multiple.' '.$s->required.'>';
+        $container = $this->html->createElement('div');
+        $this->html->appendChild($container);
+
+        $div = $this->html->createElement('div');
+        $div->setAttribute('class', 'select');
+        $container->appendChild($div);
+
+        $select = $this->html->createElement('select');
+        $select->setAttribute('type', $s['type']);
+        $select->setAttribute('label', $s['label']);
+        $select->setAttribute('id', $s['var']);
+        $select->setAttribute('name', $s['var']);
+
+        if($s->required)
+            $select->setAttribute('required', 'required');
+        
+        $div->appendChild($select);
 
         if(count($s->xpath('option')) > 0){
             foreach($s->option as $option){
-                $this->html .= '
-                    <option value="'.$option->value.'"';
-                    if(in_array((string)$option->value, $s->xpath('value')))
-                        $this->html .= ' selected';
-                $this->html .= '>'.
-                        $option->value.'
-                    </option>';
+                $opt = $this->html->createElement('option', $option->attributes()->label);
+                $opt->setAttribute('value', $option->value);
+                if(in_array((string)$opt->value, $s->xpath('value')))
+                    $select->setAttribute('selected', 'selected');
+                $select->appendChild($opt);
             }
         }
         else{
             foreach($s->value as $option){
-                $this->html .= '
-                    <option value="'.$option['label'].'" selected>'.
-                        $option.'
-                    </option>';
+                $option = $this->html->createElement('option', $option);
+                $select->setAttribute('value', $option['label']);
+                $select->setAttribute('selected', 'selected');
+                $select->appendChild($option);
             }
         }
 
-        $this->html .= '
-                </select>
-            </div>
-            <label for="'.$s["var"].'">'.$s["label"].'</label>';
+        $label = $this->html->createElement('label', $s['label']);
+        $label->setAttribute('for', $s['var']);
+        $container->appendChild($label);
     }
 }
 
-class FormtoXMPP{
-    private $stream;
-    private $inputs;
-    private $dataform;
+class FormtoXMPP
+{
+    private $_form;
+    private $_inputs;
 
-    public function __construct(){
-        $this->stream = '';
-        $this->inputs = array();
-        $this->dataform = true;
+    public function __construct(array $inputs)
+    {
+        $this->_form = new \DOMDocument('1.0', 'UTF-8');
+        $this->_inputs = $inputs;
     }
 
-    public function getXMPP($stream, $inputs){
-        $this->setXMPP($stream);
-        $this->setInputs($inputs);
-        $this->create();
-        return $this->stream;
-    }
-
-    public function setXMPP($stream){
-        $this->stream = new SimpleXMLElement($stream);
-    }
-    public function setInputs($inputs){
-        $this->inputs = $inputs;
-    }
-
-    public function setDataformOff() {
-        $this->dataform = false;
-    }
-
-    public function create(){
-        switch($this->stream->getName()){
-            case "stream":
-                $node = $this->stream->iq->query->x;
-                break;
-            case "pubsub":
-                $node = $this->stream->configure->x;
-                break;
-            case "command":
-                $node = $this->stream->x;
-                break;
-            default:
-                $node = $this->stream->x;
-                break;
+    public function appendToX(\DomDocument $dom)
+    {
+        $fields = $this->_form->getElementsByTagName('field');
+        $list = $dom->getElementsByTagName('x');
+            
+        foreach($fields as $field) {
+            $field = $dom->importNode($field, true);
+            $list[0]->appendChild($field);
         }
-        foreach($this->inputs as $key => $value) {
-            if($value === '' && $this->stream->getName() == "stream") {
-                RPC::call('movim_reload', Route::urlize('account','datamissing'));
-                RPC::commit();
-                 exit;
-            } elseif(substr($key, 0, 8) == 'generic_') {
-                $key = str_replace('generic_', '', $key);
-                if (!is_string($value)) {
-                    $value = $value->value;
-                }
-                $node->addChild($key, $value);
-            } elseif($value->attributes) {
-                $field = $node->addChild('field');
-                if($value == 'true' || $value === 1)
-                    $value = '1';
-                if($value == 'false' || $value === 0)
-                    $value = '0';
+    }
 
-                $field->addChild('value', trim($value->value));
-                if(isset($value->attributes->required))
-                    $field->addChild('required', '');
-                $field->addAttribute('var', $value->attributes->name);
-                $field->addAttribute('type', $value->attributes->xmpptype);
-                $field->addAttribute('label', $value->attributes->xmpplabel);
+    public function create()
+    {
+        foreach($this->_inputs as $key => $value) {
+            $container = $this->_form->createElement('container');
+            $this->_form->appendChild($container);
+           
+            $field = $this->_form->createElement('field');
+            $container->appendChild($field);
 
-            } else{
-                $field = $node->addChild('field');
+            $val = $this->_form->createElement('value');
+            $field->appendChild($val);
 
-                if($value == 'true')
-                    $value = '1';
-                if($value == 'false')
-                    $value = '0';
-
-                if(is_bool($value)) {
-                    $value = ($value) ? '1' : '0';
-                }
-
-                $field->addChild('value', trim($value));
-                $field->addAttribute('var', trim($key));
+            if($value === 'true') {
+                $val->nodeValue = '1';
             }
+            
+            if($value === 'false') {
+                $val->nodeValue = '0';
+            }
+
+            if(is_bool($value)) {
+                $val->nodeValue = ($value) ? '1' : '0';
+            }
+
+            if(empty($val->nodeValue)
+            && $value !== 'false' // WTF PHP !!!
+            ) {
+                $val->nodeValue = trim($value);
+            }
+            
+            $field->setAttribute('var', trim($key));
         }
+
+        return $this->_form->saveXML();
     }
 }
 ?>
