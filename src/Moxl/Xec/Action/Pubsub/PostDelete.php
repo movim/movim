17f645398d2cf0736ed52d4717e11105e88da9d4
Timothée Jaussoin <edhelas@movim.eu>
2016-03-10 22:26:13 +0100

    Delete the tags when deleting a post

--- src/Moxl/Xec/Action/Pubsub/PostDelete.php
+++ src/Moxl/Xec/Action/Pubsub/PostDelete.php
@@ -1,25 +1,25 @@
 <?php
 /*
  * PostDelete.php
- * 
+ *
  * Copyright 2012 edhelas <edhelas@edhelas-laptop>
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301, USA.
- * 
- * 
+ *
+ *
  */
 
 namespace Moxl\Xec\Action\Pubsub;
@@ -33,13 +33,13 @@ class PostDelete extends Errors
     private $_to;
     private $_id;
     private $_node;
-    
-    public function request() 
+
+    public function request()
     {
         $this->store();
         Pubsub::postDelete($this->_to, $this->_node, $this->_id);
     }
-    
+
     public function setTo($to)
     {
         $this->_to = $to;
@@ -51,20 +51,23 @@ class PostDelete extends Errors
         $this->_id = $id;
         return $this;
     }
-    
+
     public function setNode($node)
     {
         $this->_node = $node;
         return $this;
     }
-    
+
     public function handle($stanza, $parent = false) {
         $pd = new \modl\PostnDAO();
         $pd->delete($this->_id);
 
+        $td = new \Modl\TagDAO;
+        $td->delete($this->_id);
+
         $this->deliver();
     }
-    
+
     public function error($stanza) {
         $pd = new \modl\PostnDAO();
         $pd->delete($this->_id);
