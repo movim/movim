efb1f45d9a7307171285551854f0e76f9150f214
Timothée Jaussoin <edhelas@movim.eu>
2016-04-01 22:29:11 +0200

    Remove a unused function

--- app/models/conference/ConferenceDAO.php
+++ app/models/conference/ConferenceDAO.php
@@ -12,10 +12,10 @@ class ConferenceDAO extends SQL {
                 status      = :status
             where jid       = :jid
               and conference= :conference';
-        
+
         $this->prepare(
-            'Conference', 
-            array(                
+            'Conference',
+            array(
                 'jid'           => $c->jid,
                 'conference'    => $c->conference,
                 'name'          => $c->name,
@@ -24,17 +24,17 @@ class ConferenceDAO extends SQL {
                 'status'        => $c->status
             )
         );
-        
+
         $this->run('Conference');
-        
+
         if(!$this->_effective) {
             $this->_sql = '
                 insert into conference
                 (jid, conference, name, nick, autojoin, status)
                 values (:jid, :conference, :name, :nick, :autojoin, :status)';
-            
+
             $this->prepare(
-                'Conference', 
+                'Conference',
                 array(
                     'jid'           => $c->jid,
                     'conference'    => $c->conference,
@@ -44,7 +44,7 @@ class ConferenceDAO extends SQL {
                     'status'        => $c->status
                 )
             );
-            
+
             $this->run('Conference');
         }
     }
@@ -54,110 +54,63 @@ class ConferenceDAO extends SQL {
             select * from conference
             where jid       = :jid
               and conference= :conference';
-        
+
         $this->prepare(
-            'Conference', 
+            'Conference',
             array(
                 'jid' => $this->_user,
                 'conference' => $conference,
             )
         );
-        
-        return $this->run('Conference', 'item');        
+
+        return $this->run('Conference', 'item');
     }
-    
+
     function getAll() {
         $this->_sql = '
             select * from conference
             where jid = :jid
             order by conference';
-        
+
         $this->prepare(
-            'Conference', 
+            'Conference',
             array(
                 'jid' => $this->_user
             )
         );
-        
-        return $this->run('Conference');        
+
+        return $this->run('Conference');
     }
-    
-    function getConnected() {
-        $this->_sql = '
-            select * from conference
-            where jid   = :jid
-              and status= 1';
-        
-        $this->prepare(
-            'Conference', 
-            array(
-                'jid' => $this->_user
-            )
-        );
-        
-        return $this->run('Conference');        
-    }
-    /*
-    function getSubscribed() {
-        $this->_sql = '
-            select 
-                subscription.jid, 
-                subscription.server, 
-                subscription.node, 
-                subscription, 
-                name
-            from subscription
-            left outer join item 
-                on item.server = subscription.server 
-                and item.node = subscription.node
-            where subscription.jid = :jid
-            group by 
-                subscription.server, 
-                subscription.node, 
-                subscription.jid, 
-                subscription, item.name
-            order by 
-                subscription.server';
-        
-        $this->prepare(
-            'Subscription', 
-            array(
-                'jid' => $this->_user
-            )
-        );
-        
-        return $this->run('Subscription');
-    }
-    */
+
     function delete() {
         $this->_sql = '
             delete from conference
             where jid = :jid';
-        
+
         $this->prepare(
-            'Subscription', 
+            'Subscription',
             array(
                 'jid' => $this->_user
             )
         );
-        
+
         return $this->run('conference');
     }
-    
+
     function deleteNode($conference) {
         $this->_sql = '
             delete from conference
             where jid       = :jid
               and conference= :conference';
-        
+
         $this->prepare(
-            'Conference', 
+            'Conference',
             array(
                 'jid' => $this->_user,
                 'conference' => $conference
             )
         );
-        
+
         return $this->run('Conference');
     }
 }
