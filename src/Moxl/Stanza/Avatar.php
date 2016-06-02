694ae05cadc2395c8c00963c8f8f62fa1cbef7d5
Timothée Jaussoin <edhelas@movim.eu>
2016-06-02 23:06:55 +0200

    Fix a wrong sha1 calculation for XEP-0084 - Avatar

--- src/Moxl/Stanza/Avatar.php
+++ src/Moxl/Stanza/Avatar.php
@@ -19,17 +19,19 @@ class Avatar {
     static function set($data)
     {
         $dom = new \DOMDocument('1.0', 'UTF-8');
-        $pubsub = $dom->createElementNS('http://jabber.org/protocol/pubsub', 'pubsub');
+        $pubsub = $dom->createElement('pubsub');
+        $pubsub->setAttribute('xmlns', 'http://jabber.org/protocol/pubsub');
 
         $publish = $dom->createElement('publish');
         $publish->setAttribute('node', 'urn:xmpp:avatar:data');
         $pubsub->appendChild($publish);
 
         $item = $dom->createElement('item');
-        $item->setAttribute('id', sha1($data));
+        $item->setAttribute('id', sha1(base64_decode($data)));
         $publish->appendChild($item);
 
-        $data = $dom->createElementNS('urn:xmpp:avatar:data', 'data', $data);
+        $data = $dom->createElement('data', $data);
+        $data->setAttribute('xmlns', 'urn:xmpp:avatar:data');
         $item->appendChild($data);
 
         $xml = \Moxl\API::iqWrapper($pubsub, false, 'set');
@@ -38,26 +40,30 @@ class Avatar {
 
     static function setMetadata($data)
     {
+        $decoded = base64_decode($data);
+
         $dom = new \DOMDocument('1.0', 'UTF-8');
-        $pubsub = $dom->createElementNS('http://jabber.org/protocol/pubsub', 'pubsub');
+        $pubsub = $dom->createElement('pubsub');
+        $pubsub->setAttribute('xmlns', 'http://jabber.org/protocol/pubsub');
 
         $publish = $dom->createElement('publish');
         $publish->setAttribute('node', 'urn:xmpp:avatar:metadata');
         $pubsub->appendChild($publish);
 
         $item = $dom->createElement('item');
-        $item->setAttribute('id', sha1($data));
+        $item->setAttribute('id', sha1($decoded));
         $publish->appendChild($item);
 
-        $metadata = $dom->createElementNS('urn:xmpp:avatar:metadata', 'metadata');
+        $metadata = $dom->createElement('metadata');
+        $metadata->setAttribute('xmlns', 'http://jabber.org/protocol/pubsub');
         $item->appendChild($metadata);
 
         $info = $dom->createElement('info');
         $info->setAttribute('height', '410');
         $info->setAttribute('width', '410');
         $info->setAttribute('type', 'image/jpeg');
-        $info->setAttribute('id', sha1($data));
-        $info->setAttribute('bytes', strlen(base64_decode($data)));
+        $info->setAttribute('id', sha1($decoded));
+        $info->setAttribute('bytes', strlen($decoded));
         $metadata->appendChild($info);
 
         $xml = \Moxl\API::iqWrapper($pubsub, false, 'set');
