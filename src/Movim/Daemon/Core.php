2a3ddf69583699f4039582ffc5502536418d73ac
Timothée Jaussoin <edhelas@movim.eu>
2016-07-26 19:15:54 +0200

    Remove useless lines in the nginx generated config

--- src/Movim/Daemon/Core.php
+++ src/Movim/Daemon/Core.php
@@ -52,8 +52,6 @@ class Core implements MessageComponentInterface {
     proxy_set_header X-Real-IP \$remote_addr;
     proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
     proxy_set_header X-Forwarded-Proto https;
-    proxy_read_timeout 86400s;
-    proxy_send_timeout 86400s;
     proxy_redirect off;
 }
 
