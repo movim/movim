86bde999cb35cdcb9217590efdba8503dd206a63
Timothée Jaussoin <edhelas@movim.eu>
2016-07-26 22:45:31 +0200

    Update PostnDAO->exists to handle correctly the incoming Pubsub items

--- app/models/postn/PostnDAO.php
+++ app/models/postn/PostnDAO.php
@@ -654,18 +654,23 @@ class PostnDAO extends SQL {
         return $this->run('ContactPostn');
     }
 
-    function exist($id)
+    function exists($origin, $node, $id)
     {
         $this->_sql = '
             select count(*) from postn
-            where postn.nodeid = :nodeid
+            where
+                origin = :origin,
+                node = :node,
+                nodeid = :nodeid
             ';
 
         $this->prepare(
             'Postn',
-            array(
+            [
+                'origin'    => $origin,
+                'node'      => $node,
                 'nodeid'    => $id
-            )
+            ]
         );
 
         $arr = $this->run(null, 'array');
