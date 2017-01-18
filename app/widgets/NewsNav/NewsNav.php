a85a87b41f5a90959ee76bad4b2d98324ee93c20
Timothée Jaussoin <edhelas@movim.eu>
2017-01-18 23:38:54 +0100

    Randomize a bit the posts displayed in NewsNav

--- app/widgets/NewsNav/NewsNav.php
+++ app/widgets/NewsNav/NewsNav.php
@@ -4,25 +4,24 @@ class NewsNav extends \Movim\Widget\Base
 {
     public function load()
     {
-
     }
 
     public function display()
     {
         $nd = new \Modl\PostnDAO;
-        $cd = new \Modl\ContactDAO;
 
-        $this->view->assign('presencestxt', getPresencesTxt());
-        $this->view->assign('top', $cd->getTop(6));
-        $this->view->assign('blogs', $nd->getLastBlogPublic(0, 4));
+        $blogs = $nd->getLastBlogPublic(rand(0, 5), 4);
+        shuffle($blogs);
+
+        $this->view->assign('blogs', $blogs);
 
         $count = ($this->getView() == 'news') ? 3 : 6;
         $origin = ($this->get('s') && $this->get('s') != 'subscriptions') ?
             $this->get('s') : false;
 
-        $this->view->assign('posts', $nd->getLastPublished($origin, 0, $count));
+        $posts = $nd->getLastPublished($origin, rand(0, 5), $count);
+        shuffle($posts);
 
-        $this->view->assign('me', $cd->get($this->user->getLogin()), true);
-        $this->view->assign('jid', $this->user->getLogin());
+        $this->view->assign('posts', $posts);
     }
 }
