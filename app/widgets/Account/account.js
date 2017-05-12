1e6d3046810ca3e1a6b228569930ff929540fc48
Jaussoin Timothée <edhelas@movim.eu>
2017-05-12 15:11:20 +0200

    Allow the user to clear his account on the Pod and leave it properly (for a migration)

--- app/widgets/Account/account.js
+++ app/widgets/Account/account.js
@@ -3,11 +3,6 @@ var Account = {
         var form = document.querySelector('form[name=password]');
         form.reset();
         form.querySelector('a.button').className = 'button color oppose';
-    },
-    clearAccount : function() {
-        var username = localStorage.getItem("username").replace("@", "at");
-        localStorage.removeItem(username + '_Init');
-        Presence_ajaxLogout();
     }
 }
 
