25dc494827cad2c36b0fd87749798c240f5c9fea
Timothée Jaussoin <edhelas@movim.eu>
2017-06-18 00:12:53 +0200

    Small update in the chat textarea behavior

--- app/widgets/Chat/chat.js
+++ app/widgets/Chat/chat.js
@@ -43,6 +43,9 @@ var Chat = {
         setTimeout(function() {
             var textarea = document.querySelector('#chat_textarea');
             textarea.value = localStorage.getItem(textarea.dataset.jid + '_message');
+
+            MovimUtils.textareaAutoheight(textarea);
+            Chat.adaptDiscussion();
         }, 0); // Fix Me
 
         textarea.onkeydown = function(event) {
@@ -88,16 +91,25 @@ var Chat = {
                 }
             },5000);
 
+            Chat.adaptDiscussion();
             Chat.toggleAction(this.value.length);
         };
 
         textarea.oninput = function() {
             MovimUtils.textareaAutoheight(this);
+            Chat.adaptDiscussion();
         };
 
         if(document.documentElement.clientWidth > 1024) {
             document.querySelector('#chat_textarea').focus();
         }
+    },
+    adaptDiscussion: function()
+    {
+        document.querySelector('main > section > div > div.contained').style.height =
+            'calc(100% - '+
+            document.querySelector('.chat_box').clientHeight +
+            'px)';
     },
     setTextarea: function(value)
     {
