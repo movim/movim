093d42b13eea9f77644016f5efa77e50460d1e8b
Timothée Jaussoin <edhelas@movim.eu>
2017-08-21 13:39:22 +0200

    Forgot postactions.js

---
+++ app/widgets/PostActions/postactions.js
@@ -0,0 +1,8 @@
+var PostActions = {
+    handleDelete : function(url) {
+        var page = MovimUtils.urlParts().page;
+        if(page == 'post') {
+            MovimUtils.redirect(url);
+        }
+    }
+}
