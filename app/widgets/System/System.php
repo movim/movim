135cc32d1acf1454f01aba050e59b4ba3b405222
Timothée Jaussoin <edhelas@movim.eu>
2017-10-21 17:56:28 +0200

    Add application/javascript header to prevent MIME type checking issue

--- app/widgets/System/System.php
+++ app/widgets/System/System.php
@@ -11,6 +11,7 @@ class System extends \Movim\Widget\Base
     function display()
     {
         header('Cache-Control:public, max-age=31536000');
+        header('Content-Type: application/javascript');
 
         $this->view->assign('base_host',    BASE_HOST);
         $this->view->assign('small_picture_limit', SMALL_PICTURE_LIMIT);
