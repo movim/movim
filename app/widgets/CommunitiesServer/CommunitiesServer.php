c1312e23e70375f217e2927ef7a5a3b9bef1c3fb
Timothée Jaussoin <edhelas@movim.eu>
2017-12-10 10:18:42 +0100

    Redirect to the Community on creation

--- app/widgets/CommunitiesServer/CommunitiesServer.php
+++ app/widgets/CommunitiesServer/CommunitiesServer.php
@@ -27,6 +27,7 @@ class CommunitiesServer extends \Movim\Widget\Base
 
         list($origin, $node) = array_values($packet->content);
         $this->ajaxDisco($origin);
+        $this->rpc('MovimUtils.redirect', $this->route('community', [$origin, $node]));
     }
 
     function onDisco($packet)
