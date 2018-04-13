0a79b37ca8043ff7dc36d3b7f8018e8832efe639
Timothée Jaussoin <edhelas@movim.eu>
2018-04-13 17:20:46 +0200

    Update About widget

--- app/widgets/About/About.php
+++ app/widgets/About/About.php
@@ -1,24 +1,7 @@
 <?php
 
-/**
- * @package Widgets
- *
- * @file About.php
- * This file is part of MOVIM.
- * 
- * @brief A widget which display some help 
- *
- * @author Timothée
- * 
- * See COPYING for licensing information.
- */
-
 class About extends \Movim\Widget\Base
 {
-    function load()
-    {
-    }
-
     function display()
     {
         $this->view->assign('version', APP_VERSION);
