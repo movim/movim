43c73504eafc99d805ca3fc1f9751bfc73585f29
Timothée Jaussoin <edhelas@movim.eu>
2018-05-04 00:04:06 +0200

    Add doctrine/dbal to the dependencies Change length columns in the DB Fix #637

---
+++ database/migrations/20180503215606_change_length_columns_info_presence.php
@@ -0,0 +1,31 @@
+<?php
+
+use Movim\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class ChangeLengthColumnsInfoPresence extends Migration
+{
+    public function up()
+    {
+        $this->schema->table('infos', function(Blueprint $table) {
+            $table->string('node', 256)->change();
+        });
+
+        $this->schema->table('presences', function(Blueprint $table) {
+            $table->string('resource', 256)->change();
+            $table->text('status')->change();
+        });
+    }
+
+    public function down()
+    {
+        $this->schema->table('infos', function(Blueprint $table) {
+            $table->string('node', 96)->change();
+        });
+
+        $this->schema->table('presences', function(Blueprint $table) {
+            $table->string('resource', 128)->change();
+            $table->string('status', 255)->change();
+        });
+    }
+}
