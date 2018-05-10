9b084d1fb4b2bb2504043e029b9663a99a26e24f
Timothée Jaussoin <edhelas@movim.eu>
2018-05-10 23:59:29 +0200

    Fix warning in Vcard

--- src/Moxl/Stanza/Vcard.php
+++ src/Moxl/Stanza/Vcard.php
@@ -17,18 +17,18 @@ class Vcard
         $dom = new \DOMDocument('1.0', 'UTF-8');
         $vcard = $dom->createElementNS('vcard-temp', 'vCard');
 
-        if ($data->fn) $vcard->appendChild($dom->createElement('FN', $data->fn->value));
-        if ($data->name) $vcard->appendChild($dom->createElement('NICKNAME', $data->name->value));
-        if ($data->url) $vcard->appendChild($dom->createElement('URL', $data->url->value));
-        if ($data->date) $vcard->appendChild($dom->createElement('BDAY', $data->date->value));
+        if (isset($data->fn)) $vcard->appendChild($dom->createElement('FN', $data->fn->value));
+        if (isset($data->name)) $vcard->appendChild($dom->createElement('NICKNAME', $data->name->value));
+        if (isset($data->url)) $vcard->appendChild($dom->createElement('URL', $data->url->value));
+        if (isset($data->date)) $vcard->appendChild($dom->createElement('BDAY', $data->date->value));
 
-        if ($data->email) {
+        if (isset($data->email)) {
             $email = $dom->createElement('EMAIL');
             $email->appendChild($dom->createElement('USERID', $data->email->value));
             $vcard->appendChild($email);
         }
 
-        if ($data->country || $data->locality || $data->postalcode) {
+        if (isset($data->country) || isset($data->locality) || isset($data->postalcode)) {
             $adr = $dom->createElement('ADR');
             $adr->appendChild($dom->createElement('LOCALITY', $data->locality->value));
             $adr->appendChild($dom->createElement('PCODE', $data->postalcode->value));
@@ -36,9 +36,9 @@ class Vcard
             $vcard->appendChild($adr);
         }
 
-        if ($data->desc) $vcard->appendChild($dom->createElement('DESC', $data->desc->value));
+        if (isset($data->desc)) $vcard->appendChild($dom->createElement('DESC', $data->desc->value));
 
-        if ($data->photobin && $data->phototype) {
+        if (isset($data->photobin) && isset($data->phototype)) {
             $photo = $dom->createElement('PHOTO');
             $photo->appendChild($dom->createElement('TYPE', $data->phototype->value));
             $photo->appendChild($dom->createElement('BINVAL', $data->photobin->value));
