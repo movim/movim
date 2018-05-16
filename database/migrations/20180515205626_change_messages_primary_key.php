ab9c3eab8a25d3e112b656439f76a326f464db21
Timothée Jaussoin <edhelas@movim.eu>
2018-05-16 19:23:47 +0200

    Fix last migration for MySQL

--- database/migrations/20180515205626_change_messages_primary_key.php
+++ database/migrations/20180515205626_change_messages_primary_key.php
@@ -7,17 +7,25 @@ class ChangeMessagesPrimaryKey extends Migration
 {
     public function up()
     {
+        $this->disableForeignKeyCheck();
+
         $this->schema->table('messages', function(Blueprint $table) {
             $table->dropPrimary('messages_pkey');
             $table->primary(['user_id', 'jidfrom', 'id']);
         });
+
+        $this->enableForeignKeyCheck();
     }
 
     public function down()
     {
+        $this->disableForeignKeyCheck();
+
         $this->schema->table('messages', function(Blueprint $table) {
             $table->dropPrimary('messages_pkey');
             $table->primary(['user_id', 'id']);
         });
+
+        $this->enableForeignKeyCheck();
     }
 }
