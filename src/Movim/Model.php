893b41f65c002d69e460f14c823c7764cecd3824
Timothée Jaussoin <edhelas@movim.eu>
2018-05-18 11:17:18 +0200

    Add Movim/Model

---
+++ src/Movim/Model.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Movim;
+
+use Exception;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model as EloquentModel;
+
+class Model extends EloquentModel
+{
+    /**
+     * Get the value indicating whether the IDs are incrementing.
+     *
+     * @return bool
+     */
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    /**
+     * Set the keys for a save update query.
+     *
+     * @param  Builder $query
+     *
+     * @return Builder
+     * @throws Exception
+     */
+    protected function setKeysForSaveQuery(Builder $query)
+    {
+        foreach ($this->getKeyName() as $key) {
+            if ( ! isset($this->$key)) {
+                throw new Exception(__METHOD__ . 'Missing part of the primary key: ' . $key);
+            }
+
+            $query->where($key, '=', $this->$key);
+        }
+
+        return $query;
+    }
+
+    /**
+     * Execute a query for a single record by ID.
+     *
+     * @param  array $ids Array of keys, like [column => value].
+     * @param  array $columns
+     *
+     * @return mixed|static
+     */
+    public static function find($ids, $columns = ['*'])
+    {
+        $me    = new self;
+        $query = $me->newQuery();
+        foreach ($me->getKeyName() as $key) {
+            $query->where($key, '=', $ids[$key]);
+        }
+
+        return $query->first($columns);
+    }
+}
