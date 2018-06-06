87cf4d18e9ee934e3ce99497f1306d6a05b0c906
Timothée Jaussoin <edhelas@movim.eu>
2018-06-06 18:13:10 +0200

    Only index jidto

--- database/migrations/20180606155549_add_jid_to_indexes_to_messages.php
+++ database/migrations/20180606155549_add_jid_to_indexes_to_messages.php
@@ -3,19 +3,19 @@
 use Movim\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class AddJidToJidFromIndexesToMessages extends Migration
+class AddJidToIndexesToMessages extends Migration
 {
     public function up()
     {
         $this->schema->table('messages', function(Blueprint $table) {
-            $table->index(['user_id', 'jidfrom', 'jidto', 'id'])->nullable();
+            $table->index('jidto');
         });
     }
 
     public function down()
     {
         $this->schema->table('messages', function(Blueprint $table) {
-            $table->dropIndex('messages_user_id_jidfrom_jidto_id_index');
+            $table->dropIndex('messages_jidto_index');
         });
 
     }
