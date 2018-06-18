05b568b0ade2da30e94e05257fc63b3d634f35cf
Timothée Jaussoin <edhelas@movim.eu>
2018-06-18 23:29:17 +0200

    Add indexes on attachements as well

---
+++ database/migrations/20180618212639_add_category_and_rel_indexes_to_attachments.php
@@ -0,0 +1,23 @@
+<?php
+
+use Movim\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddCategoryAndRelIndexesToAttachments extends Migration
+{
+    public function up()
+    {
+        $this->schema->table('attachments', function(Blueprint $table) {
+            $table->index('category');
+            $table->index('rel');
+        });
+    }
+
+    public function down()
+    {
+        $this->schema->table('attachments', function(Blueprint $table) {
+            $table->dropIndex('attachments_category_index');
+            $table->dropIndex('attachments_rel_index');
+        });
+    }
+}
