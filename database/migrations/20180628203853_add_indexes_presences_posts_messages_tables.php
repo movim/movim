7290fac0ecaa80393fcd767acdcf940d055e9df1
Timothée Jaussoin <edhelas@movim.eu>
2018-06-28 23:36:09 +0200

    Add a new migration to index more columns in the DB

---
+++ database/migrations/20180628203853_add_indexes_presences_posts_messages_tables.php
@@ -0,0 +1,45 @@
+<?php
+
+use Movim\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddIndexesPresencesPostsMessagesTables extends Migration
+{
+    public function up()
+    {
+        $this->schema->table('presences', function(Blueprint $table) {
+            $table->index('jid');
+            $table->index('resource');
+        });
+
+        $this->schema->table('messages', function(Blueprint $table) {
+            $table->index('jidfrom');
+            $table->index('published');
+        });
+
+        $this->schema->table('posts', function(Blueprint $table) {
+            $table->index('server');
+            $table->index('node');
+            $table->index('published');
+        });
+    }
+
+    public function down()
+    {
+        $this->schema->table('presences', function(Blueprint $table) {
+            $table->dropIndex('presences_jid_index');
+            $table->dropIndex('presences_resource_index');
+        });
+
+        $this->schema->table('messages', function(Blueprint $table) {
+            $table->dropIndex('messages_jidfrom_index');
+            $table->dropIndex('messages_published_index');
+        });
+
+        $this->schema->table('posts', function(Blueprint $table) {
+            $table->dropIndex('posts_server_index');
+            $table->dropIndex('posts_node_index');
+            $table->dropIndex('posts_published_index');
+        });
+    }
+}
