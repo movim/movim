66418e220e057d5554f7bf924d180b6340750e4d
Timothée Jaussoin <edhelas@movim.eu>
2018-08-03 11:05:16 +0200

    Don't update the date of the messages when they are edited Display the contact avatars again when having a day separator between two messages

--- app/Message.php
+++ app/Message.php
@@ -90,7 +90,7 @@ class Message extends Model
             $this->published = gmdate('Y-m-d H:i:s', strtotime($stanza->delay->attributes()->stamp));
         } elseif ($parent && $parent->delay) {
             $this->published = gmdate('Y-m-d H:i:s', strtotime($parent->delay->attributes()->stamp));
-        } else {
+        } elseif (!isset($stanza->replace)) {
             $this->published = gmdate('Y-m-d H:i:s');
         }
 
