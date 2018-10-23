3748b31e583f4bafbf01e057e07cf2c3971c86bd
Timothée Jaussoin <edhelas@movim.eu>
2018-10-23 08:37:06 +0200

    Don't display/count offline presences in MUC

--- app/Conference.php
+++ app/Conference.php
@@ -36,6 +36,7 @@ class Conference extends Model
         return $this->hasMany('App\Presence', 'jid', 'conference')
                     ->where('session_id', $this->session_id)
                     ->where('resource', '!=', '')
+                    ->where('value', '<', 5)
                     ->orderBy('mucaffiliation', 'desc')
                     ->orderBy('value');
     }
