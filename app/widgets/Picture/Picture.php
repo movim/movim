dd0a45a3df31bcc959d6ad9060c189d26427836c
Timothée Jaussoin <edhelas@movim.eu>
2018-12-12 22:11:02 +0100

    Update the dependencies Proxy the pictures if they are served over HTTP + add a one day cache

--- app/widgets/Picture/Picture.php
+++ app/widgets/Picture/Picture.php
@@ -1,6 +1,7 @@
 <?php
 
 use Respect\Validation\Validator;
+use Movim\Picture as PictureManager;
 
 class Picture extends \Movim\Widget\Base
 {
@@ -15,9 +16,42 @@ class Picture extends \Movim\Widget\Base
             && $headers["download_content_length"] <= SMALL_PICTURE_LIMIT
             && $headers["download_content_length"] > 2000
             && typeIsPicture($headers['content_type'])) {
-                header("HTTP/1.1 301 Moved Permanently");
-                header('Location: ' . $url);
-                return;
+                $components = parse_url($url);
+
+                // We proxify the picture if served from HTTP
+                if ($components['scheme'] === 'http') {
+                    $ch = curl_init();
+                    curl_setopt($ch, CURLOPT_URL, $url);
+                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                    curl_setopt($ch, CURLOPT_HEADER, true);
+                    curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
+                    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+                    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 4);
+                    curl_setopt($ch, CURLOPT_BUFFERSIZE, 12800);
+                    curl_setopt($ch, CURLOPT_NOPROGRESS, false);
+                    curl_setopt($ch, CURLOPT_PROGRESSFUNCTION, function($downloadSize, $downloaded, $uploadSize, $uploaded) {
+                        return ($downloaded > SMALL_PICTURE_LIMIT) ? 1 : 0;
+                    });
+
+                    list($header, $contents) = preg_split('/([\r\n][\r\n])\\1/', curl_exec($ch), 2);
+                    curl_close ($ch);
+
+                    $headers = preg_split('/[\r\n]+/', $header);
+
+                    foreach ($headers as $header) {
+                        if (preg_match('/^(?:Content-Type|Content-Language|Set-Cookie):/i', $header)) {
+                            header($header);
+                        }
+                    }
+
+                    header('Cache-Control:public, max-age=' . 3600*24);
+                    print $contents;
+                    return;
+                } else {
+                    header("HTTP/1.1 301 Moved Permanently");
+                    header('Location: ' . $url);
+                    return;
+                }
             }
 
             header("HTTP/1.1 301 Moved Permanently");
