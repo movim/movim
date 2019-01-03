81a88436d946217a0142cc9764eeabdf1b7eaa72
Timothée Jaussoin <edhelas@movim.eu>
2019-01-03 23:21:14 +0100

    Also exclude the admin page from the list page redirected when the linker is started

--- app/assets/js/movim_websocket.js
+++ app/assets/js/movim_websocket.js
@@ -85,7 +85,7 @@ var MovimWebsocket = {
 
                 if (obj.func == 'started') {
                     // If the linker was started but we're not on the login page
-                    if (!['login', 'account', 'accountnext', 'tag'].includes(MovimUtils.urlParts().page)) {
+                    if (!['login', 'account', 'accountnext', 'tag', 'admin'].includes(MovimUtils.urlParts().page)) {
                         MovimUtils.disconnect();
                     } else {
                         MovimWebsocket.launchStarted();
