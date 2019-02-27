a9458dd75a000cc5fd51702013eb5b885aed0d83
Timothée Jaussoin <edhelas@movim.eu>
2019-02-27 23:13:41 +0100

    Actually fix #801 (forgot the migration file)

---
+++ database/migrations/20190224220950_change_length_columns_conferences_rosters_users.php
@@ -0,0 +1,39 @@
+<?php
+
+use Movim\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class ChangeLengthColumnsConferencesRostersUsers extends Migration
+{
+    public function up()
+    {
+        $this->schema->table('conferences', function(Blueprint $table) {
+            $table->string('name', 512)->change();
+            $table->string('nick', 256)->change();
+        });
+
+        $this->schema->table('rosters', function(Blueprint $table) {
+            $table->string('name', 256)->change();
+        });
+
+        $this->schema->table('users', function(Blueprint $table) {
+            $table->string('cssurl', 256)->change();
+        });
+    }
+
+    public function down()
+    {
+        $this->schema->table('conferences', function(Blueprint $table) {
+            $table->string('name', 128)->change();
+            $table->string('nick', 128)->change();
+        });
+
+        $this->schema->table('rosters', function(Blueprint $table) {
+            $table->string('name', 128)->change();
+        });
+
+        $this->schema->table('users', function(Blueprint $table) {
+            $table->string('cssurl', 128)->change();
+        });
+    }
+}
