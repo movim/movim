809a6bc997eac29a8fb1a9ea1c4dadea707732bd
Timothée Jaussoin <edhelas@movim.eu>
2019-04-12 11:55:15 +0200

    Fix latest migration for SQLite

--- database/migrations/20190324190611_create_reactions_table.php
+++ database/migrations/20190324190611_create_reactions_table.php
@@ -11,10 +11,65 @@ class CreateReactionsTable extends Migration
             $table->dropPrimary('messages_pkey');
         });
 
-        $this->schema->table('messages', function (Blueprint $table) {
-            $table->increments('mid');
-            $table->unique(['user_id', 'jidfrom', 'id']);
-        });
+        // Let's just recreate the full table for SQLite…
+        if ($this->schema->getConnection()->getDriverName() == 'sqlite') {
+            $this->schema->create('messages_tmp', function (Blueprint $table) {
+                $table->string('user_id', 256);
+                $table->string('id', 255);
+                $table->string('oldid', 255)->nullable();
+                $table->string('type', 255);
+                $table->text('subject')->nullable();
+                $table->string('thread', 255)->nullable();
+                $table->text('body')->nullable();
+                $table->text('html')->nullable();
+                $table->datetime('published');
+                $table->datetime('delivered')->nullable();
+                $table->datetime('displayed')->nullable();
+                $table->boolean('quoted')->default(false);
+                $table->boolean('markable')->default(false);
+                $table->text('picture')->nullable();
+                $table->string('sticker', 255)->nullable();
+                $table->text('file')->nullable();
+                $table->timestamps();
+                $table->string('jidto', 256);
+                $table->string('jidfrom', 256);
+                $table->string('resource', 256)->nullable();
+                $table->string('replaceid', 255)->nullable();
+                $table->increments('mid');
+            });
+
+            $this->schema->getConnection()->unprepared('
+                insert into messages_tmp(
+                        user_id, id, oldid, type, subject, thread, body, html, published,
+                        delivered, displayed, quoted, markable, picture, sticker, file,
+                        created_at, updated_at, jidto, jidfrom, resource, replaceid)
+                select user_id, id, oldid, type, subject, thread, body, html, published,
+                        delivered, displayed, quoted, markable, picture, sticker, file,
+                        created_at, updated_at, jidto, jidfrom, resource, replaceid
+                from messages');
+
+            $this->schema->drop('messages');
+            $this->schema->rename('messages_tmp', 'messages');
+
+            $this->schema->table('messages', function (Blueprint $table) {
+                $table->unique(['user_id', 'jidfrom', 'id']);
+
+                $table->index('jidto');
+                $table->index('jidfrom');
+                $table->index('published');
+                $table->index('replaceid');
+                $table->index('type');
+
+                $table->foreign('user_id')
+                    ->references('id')->on('users')
+                    ->onDelete('cascade');
+            });
+        } else {
+            $this->schema->table('messages', function (Blueprint $table) {
+                $table->increments('mid');
+                $table->unique(['user_id', 'jidfrom', 'id']);
+            });
+        }
 
         $this->schema->create('reactions', function (Blueprint $table) {
             $table->integer('message_mid');
@@ -34,10 +89,10 @@ class CreateReactionsTable extends Migration
         $this->schema->drop('reactions');
 
         $this->schema->table('messages', function (Blueprint $table) {
-            $table->dropUnique('messages_user_id_jidfrom_id_unique');
             $table->dropPrimary('messages_pkey');
             $table->dropColumn('mid');
             $table->primary(['user_id', 'jidfrom', 'id']);
+            $table->dropUnique('messages_user_id_jidfrom_id_unique');
         });
     }
 }
