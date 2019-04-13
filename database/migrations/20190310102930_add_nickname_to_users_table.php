476710d417a0972f51d0b5ab76db09a49a0e29cc
Timothée Jaussoin <edhelas@movim.eu>
2019-04-13 19:18:05 +0200

    Fix nickname migration for MySQL

--- database/migrations/20190310102930_add_nickname_to_users_table.php
+++ database/migrations/20190310102930_add_nickname_to_users_table.php
@@ -7,17 +7,25 @@ class AddNicknameToUsersTable extends Migration
 {
     public function up()
     {
+        $this->disableForeignKeyCheck();
+
         $this->schema->table('users', function (Blueprint $table) {
             $table->string('nickname', 64)->nullable();
             $table->unique('nickname');
         });
+
+        $this->enableForeignKeyCheck();
     }
 
     public function down()
     {
+        $this->disableForeignKeyCheck();
+
         $this->schema->table('users', function (Blueprint $table) {
             $table->dropUnique('users_nickname_unique');
             $table->dropColumn('nickname');
         });
+
+        $this->enableForeignKeyCheck();
     }
 }
