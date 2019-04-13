1c3985da884f47e9f35a1c54c613f06948cdd090
Timothée Jaussoin <edhelas@movim.eu>
2019-04-13 19:31:47 +0200

    Fix reactions migration for MySQL

--- database/migrations/20190324190611_create_reactions_table.php
+++ database/migrations/20190324190611_create_reactions_table.php
@@ -7,6 +7,8 @@ class CreateReactionsTable extends Migration
 {
     public function up()
     {
+        $this->disableForeignKeyCheck();
+
         $this->schema->table('messages', function (Blueprint $table) {
             $table->dropPrimary('messages_pkey');
         });
@@ -72,20 +74,24 @@ class CreateReactionsTable extends Migration
         }
 
         $this->schema->create('reactions', function (Blueprint $table) {
-            $table->integer('message_mid');
-            $table->foreign('message_mid')->references('mid')
-                  ->on('messages')->onDelete('cascade');
+            $table->integer('message_mid')->unsigned();
             $table->string('jidfrom', 256);
             $table->string('emoji', 1);
-
             $table->timestamps();
+
+            $table->foreign('message_mid')->references('mid')
+            ->on('messages')->onDelete('cascade');
 
             $table->unique(['message_mid', 'jidfrom', 'emoji']);
         });
+
+        $this->enableForeignKeyCheck();
     }
 
     public function down()
     {
+        $this->disableForeignKeyCheck();
+
         $this->schema->drop('reactions');
 
         $this->schema->table('messages', function (Blueprint $table) {
@@ -94,5 +100,7 @@ class CreateReactionsTable extends Migration
             $table->primary(['user_id', 'jidfrom', 'id']);
             $table->dropUnique('messages_user_id_jidfrom_id_unique');
         });
+
+        $this->enableForeignKeyCheck();
     }
 }
