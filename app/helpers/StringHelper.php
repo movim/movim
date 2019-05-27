1a55f4150b21bfdcba96bd24a49078dc1d9072a3
Dominik George <nik@naturalnet.de>
2019-05-27 11:08:48 +0200

    Fix braces on function definitions.

--- app/helpers/StringHelper.php
+++ app/helpers/StringHelper.php
@@ -430,7 +430,8 @@ function implodeCsv($value) {
 /**
  * Returns true if the value is an IPv6 or IPv4 address (in that order)
  */
-function isIpAddress($value) {
+function isIpAddress($value)
+{
     $ipV6Regex = '/^\[(?:(?:(?:[0-9A-Fa-f]{0,4}:){7}[0-9A-Fa-f]{0,4})|(?:(?:[0-9A-Fa-f]{0,4}:){6}:[0-9A-Fa-f]{0,4})|(?:(?:[0-9A-Fa-f]{0,4}:){5}:(?:[0-9A-Fa-f]{0,4}:)?[0-9A-Fa-f]{0,4})|(?:(?:[0-9A-Fa-f]{0,4}:){4}:(?:[0-9A-Fa-f]{0,4}:){0,2}[0-9A-Fa-f]{0,4})|(?:(?:[0-9A-Fa-f]{0,4}:){3}:(?:[0-9A-Fa-f]{0,4}:){0,3}[0-9A-Fa-f]{0,4})|(?:(?:[0-9A-Fa-f]{0,4}:){2}:(?:[0-9A-Fa-f]{0,4}:){0,4}[0-9A-Fa-f]{0,4})|(?:(?:[0-9A-Fa-f]{0,4}:){6}(?:(?:(?:25[0-5])|(?:2[0-4]\d)|(?:1\d{2})|(?:\d{1,2}))\.){3}(?:(?:25[0-5])|(?:2[0-4]\d)|(?:1\d{2})|(?:\d{1,2})))|(?:(?:[0-9A-Fa-f]{0,4}:){0,5}:(?:(?:(?:25[0-5])|(?:2[0-4]\d)|(?:1\d{2})|(?:\d{1,2}))\.){3}(?:(?:25[0-5])|(?:2[0-4]\d)|(?:1\d{2})|(?:\d{1,2})))|(?:::(?:[0-9A-Fa-f]{0,4}:){0,5}(?:(?:(?:25[0-5])|(?:2[0-4]\d)|(?:1\d{2})|(?:\d{1,2}))\.){3}(?:(?:25[0-5])|(?:2[0-4]\d)|(?:1\d{2})|(?:\d{1,2})))|(?:[0-9A-Fa-f]{0,4}::(?:[0-9A-Fa-f]{0,4}:){0,5}[0-9A-Fa-f]{0,4})|(?:::(?:[0-9A-Fa-f]{0,4}:){0,6}[0-9A-Fa-f]{0,4})|(?:(?:[0-9A-Fa-f]{0,4}:){1,7}:))\]$/';
     $ipV4Regex = '/^(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}$/';
 
@@ -441,7 +442,8 @@ function isIpAddress($value) {
  * Returns a part of a JID, stringprep'ed with the given profile,
  * or false if it is invalid under that profile.
  */
-function prepJidPart($value, $prep) {
+function prepJidPart($value, $prep)
+{
     try {
         $ret = $prep->apply($value);
     } catch (ProfileException $e) {
@@ -459,7 +461,8 @@ function prepJidPart($value, $prep) {
  * Returns a JID, stringprep'ed with the given profile,
  * or false if it is invalid under that profile.
  */
-function prepJid($value) {
+function prepJid($value)
+{
     // Split JID into parts
     $jidParts = explodeJid($value);
 
