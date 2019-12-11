8fb5a27d54d914ac314b81f7a85ef18dc7f9eac9
Timothée Jaussoin <edhelas@movim.eu>
2019-12-11 09:58:53 +0100

    Put back another e parameter in movim_utils.js

--- public/scripts/movim_utils.js
+++ public/scripts/movim_utils.js
@@ -104,7 +104,7 @@ var MovimUtils = {
 
         for(var i = 0; i < textareas.length; i++) {
             MovimUtils.textareaAutoheight(textareas[i]);
-            textareas[i].addEventListener('keyup', () => MovimUtils.textareaAutoheight(e.target));
+            textareas[i].addEventListener('keyup', e => MovimUtils.textareaAutoheight(e.target));
         };
     },
     htmlEscape: function(string) {
