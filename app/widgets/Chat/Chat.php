43df6f6174658e4e10863eec3afe705a131f3385
Timothée Jaussoin <edhelas@movim.eu>
2020-01-19 23:22:10 +0100

    Don't append html body when received file in Chat

--- app/widgets/Chat/Chat.php
+++ app/widgets/Chat/Chat.php
@@ -807,7 +807,7 @@ class Chat extends \Movim\Widget\Base
 
         if ($message->encrypted) {
             $message->body = '<i class="material-icons">lock</i> '.__('message.encrypted');
-        } elseif (isset($message->html)) {
+        } elseif (isset($message->html) && !isset($message->file)) {
             $message->body = $message->html;
         } else {
             $message->addUrls();
