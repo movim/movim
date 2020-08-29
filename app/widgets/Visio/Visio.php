9bfdb4011f6b6dddd5b56e122b5c76bcd0ff794c
Timothée Jaussoin <edhelas@movim.eu>
2020-08-29 10:53:12 +0200

    Remove some old STUN services

--- app/widgets/Visio/Visio.php
+++ app/widgets/Visio/Visio.php
@@ -216,25 +216,11 @@ class Visio extends Base
     public function setDefaultServices()
     {
         $servers = [
-            /*'stun:stun01.sipphone.com',
-            'stun:stun.ekiga.net',
-            'stun:stun.fwdnet.net',
-            'stun:stun.ideasip.com',
-            'stun:stun.iptel.org',
-            'stun:stun.rixtelecom.se',
-            'stun:stun.schlund.de',*/
             'stun:stun.l.google.com:19305',
             'stun:stun1.l.google.com:19305',
             'stun:stun2.l.google.com:19305',
             'stun:stun3.l.google.com:19305',
             'stun:stun4.l.google.com:19305',
-            /*'stun:stunserver.org',
-            'stun:stun.softjoys.com',
-            'stun:stun.voiparound.com',
-            'stun:stun.voipbuster.com',
-            'stun:stun.voipstunt.com',
-            'stun:stun.voxgratia.org',
-            'stun:stun.xten.com'*/
         ];
 
         shuffle($servers);
