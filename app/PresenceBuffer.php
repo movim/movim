dd953e5043b6730282e3ffd5e0653896976a21a3
Timothée Jaussoin <edhelas@movim.eu>
2020-10-18 22:38:55 +0200

    Fix a dumb error when processing Presence collections in PresenceBuffer

--- app/PresenceBuffer.php
+++ app/PresenceBuffer.php
@@ -40,13 +40,14 @@ class PresenceBuffer
 
                 // We delete all the presences that might already be there
                 $table = DB::table('presences');
-                $first = $this->_models->shift();
+                $first = $this->_models->first();
                 $table = $table->where([
                     ['session_id', $first['session_id']],
                     ['jid', $first['jid']],
                     ['resource', $first['resource']],
                 ]);
-                $this->_models->each(function ($presence) use ($table) {
+
+                $this->_models->skip(1)->each(function ($presence) use ($table) {
                     $table->orWhere([
                         ['session_id', $presence['session_id']],
                         ['jid', $presence['jid']],
