889b7290d0ff173c2f2bb9d7c5cf64e1877b098a
Timothée Jaussoin <edhelas@movim.eu>
2020-10-25 17:51:05 +0100

    Prevent an error when saving many bookmarks in the DB

--- lib/moxl/src/Xec/Action/Bookmark2/Get.php
+++ lib/moxl/src/Xec/Action/Bookmark2/Get.php
@@ -26,12 +26,21 @@ class Get extends Action
             ->delete();
 
         $conferences = [];
+        $conferenceIds = [];
 
         foreach ($stanza->pubsub->items->item as $c) {
             $conference = new Conference;
             $conference->set($c);
             array_push($conferences, $conference->toArray());
+            array_push($conferenceIds, $conference->conference);
         }
+
+        // We remove the conferences that might be saved under another bookmark version
+        \App\User::me()
+            ->session
+            ->conferences()
+            ->whereIn('conference', $conferenceIds)
+            ->delete();
 
         Conference::saveMany($conferences);
 
