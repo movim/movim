8159edaf053a45f5783d9fb75196eea2c62f97fc
Timothée Jaussoin <edhelas@movim.eu>
2022-01-18 23:46:29 +0100

    Add icons for PWA shortcuts Fix 512px icon size

--- app/widgets/Manifest/Manifest.php
+++ app/widgets/Manifest/Manifest.php
@@ -12,14 +12,14 @@ class Manifest extends Base
             'description'   => __('global.description'),
             'scope'         => BASE_URI,
             'icons'         => [
-                [
+                /*[
                     'src' => BASE_URI . 'theme/img/app/vectorial_square.svg',
                     'sizes' => '512x512',
                     'type' => 'image/svg+xml',
                     'purpose' => 'maskable'
-                ],
+                ],*/
                 [
-                    'src' => BASE_URI . 'theme/img/app/vectorial.png',
+                    'src' => BASE_URI . 'theme/img/app/512_square.png',
                     'sizes' => '512x512',
                     'type' => 'image/png',
                     'purpose' => 'maskable'
@@ -35,25 +35,55 @@ class Manifest extends Base
                     'sizes' => '192x192',
                     'type' => 'image/png',
                     'purpose' => 'any'
+                ],
+                [
+                    'src' => BASE_URI . 'theme/img/app/96.png',
+                    'sizes' => '96x96',
+                    'type' => 'image/png',
+                    'purpose' => 'any'
                 ]
             ],
             'shortcuts'     => [
                 [
                     'name'  => $this->__('page.chats'),
-                    'url'   => $this->route('chat')
+                    'url'   => $this->route('chat'),
+                    'icons' => [
+                        [
+                            'src' => BASE_URI . 'theme/img/app/shortcuts/chat.png',
+                            'sizes' => '96x96',
+                            'type' => 'image/png',
+                            'purpose' => 'any'
+                        ]
+                    ]
                 ],
                 [
                     'name'  => $this->__('page.publish'),
-                    'url'   => $this->route('publish')
+                    'url'   => $this->route('publish'),
+                    'icons' => [
+                        [
+                            'src' => BASE_URI . 'theme/img/app/shortcuts/publish.png',
+                            'sizes' => '96x96',
+                            'type' => 'image/png',
+                            'purpose' => 'any'
+                        ]
+                    ]
                 ],
                 [
                     'name'  => $this->__('page.news'),
-                    'url'   => $this->route('news')
+                    'url'   => $this->route('news'),
+                    'icons' => [
+                        [
+                            'src' => BASE_URI . 'theme/img/app/shortcuts/news.png',
+                            'sizes' => '96x96',
+                            'type' => 'image/png',
+                            'purpose' => 'any'
+                        ]
+                    ]
                 ]
             ],
             'display'       => 'standalone',
             'orientation'   => 'portrait-primary',
-            'background_color' => '#3F51B5',
+            'background_color' => '#222e75',
             'theme_color'   => '#10151A',
             'start_url'     => '/?login',
         ];
