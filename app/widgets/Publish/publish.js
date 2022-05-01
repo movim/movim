d981aa5fbbda46f0c30eccc2ecf720ab74929974
Timothée Jaussoin <edhelas@movim.eu>
2022-05-01 23:38:00 +0200

    Fix #1055

--- app/widgets/Publish/publish.js
+++ app/widgets/Publish/publish.js
@@ -2,58 +2,84 @@ var Publish = {
     titleTimeout: null,
     contentTimeout: null,
 
-    init: function() {
+    init: function () {
         let id = document.querySelector('#publish input[name=id]').value;
 
         Publish_ajaxTryResolveShareUrl(id);
 
-        document.querySelector('#publish textarea[name=title]').addEventListener('keyup', function(event) {
+        document.querySelector('#publish textarea[name=title]').addEventListener('keyup', function (event) {
             if (Publish.titleTimeout) clearTimeout(Publish.titleTimeout);
             document.querySelector('#publish textarea[name=title] + label span.save').classList.remove('saved');
 
             Publish.titleTimeout = setTimeout(function () {
-                Publish_ajaxHttpSaveTitle(id, event.target.value);
+                Publish.saveTitle();
             }, 1000);
         });
 
-        document.querySelector('#publish textarea[name=content]').addEventListener('keyup', function(event) {
+        document.querySelector('#publish textarea[name=content]').addEventListener('keyup', function (event) {
             if (Publish.contentTimeout) clearTimeout(Publish.contentTimeout);
             document.querySelector('#publish textarea[name=content] + label span.save').classList.remove('saved');
 
             Publish.contentTimeout = setTimeout(function () {
-                Publish_ajaxHttpSaveContent(id, event.target.value);
+                Publish.saveContent();
             }, 1000);
         });
     },
 
-    enableSend: function() {
+    saveTitle: function () {
+        let id = document.querySelector('#publish input[name=id]').value;
+        return Publish_ajaxHttpSaveTitle(id, document.querySelector('#publish textarea[name=title]').value);
+    },
+
+    saveContent: function () {
+        let id = document.querySelector('#publish input[name=id]').value;
+        return Publish_ajaxHttpSaveContent(id, document.querySelector('#publish textarea[name=content]').value);
+    },
+
+    enableSend: function () {
         document.querySelector('.button.send').classList.remove('disabled');
         document.querySelector('.button.send i').classList.remove('spin');
         document.querySelector('.button.send i').innerText = 'send';
     },
 
-    disableSend: function() {
+    disableSend: function () {
         document.querySelector('.button.send').classList.add('disabled');
         document.querySelector('.button.send i').classList.add('spin');
         document.querySelector('.button.send i').innerText = 'autorenew';
     },
 
-    preview: function() {
-        let id = document.querySelector('#publish input[name=id]').value;
-        Publish_ajaxPreview(id);
+    preview: function () {
+        Publish.saveTitle().onreadystatechange = function () {
+            if (this.readyState == 4) {
+                Publish.saveContent().onreadystatechange = function () {
+                    if (this.readyState == 4) {
+                        let id = document.querySelector('#publish input[name=id]').value;
+                        Publish_ajaxPreview(id);
+                    }
+                }
+            }
+        }
     },
 
-    publish: function() {
-        let id = document.querySelector('#publish input[name=id]').value;
-        Publish_ajaxPublish(id);
+    publish: function () {
+        Publish.saveTitle().onreadystatechange = function () {
+            if (this.readyState == 4) {
+                Publish.saveContent().onreadystatechange = function () {
+                    if (this.readyState == 4) {
+                        let id = document.querySelector('#publish input[name=id]').value;
+                        Publish_ajaxPublish(id);
+                    }
+                }
+            }
+        }
     },
 
-    clearReply: function() {
+    clearReply: function () {
         let id = document.querySelector('#publish input[name=id]').value;
         Publish_ajaxClearReply(id);
     },
 
-    addUrl: function() {
+    addUrl: function () {
         let id = document.querySelector('#publish input[name=id]').value;
         var url = document.querySelector('#url').value;
 
@@ -61,13 +87,13 @@ var Publish = {
     }
 }
 
-MovimWebsocket.attach(function() {
+MovimWebsocket.attach(function () {
     Publish.init();
     MovimUtils.applyAutoheight();
 });
 
 if (typeof Upload != 'undefined') {
-    Upload.attach(function(file) {
+    Upload.attach(function (file) {
         let id = document.querySelector('#publish input[name=id]').value;
         Publish_ajaxAddEmbed(id, file.uri);
     });
