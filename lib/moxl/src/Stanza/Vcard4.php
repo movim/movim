227966333f820f36a2b64922ea0516d5adad2f39
Timothée Jaussoin <edhelas@movim.eu>
2022-10-10 22:46:35 +0200

    Fix #1036 add publish-options for Vcard4

--- lib/moxl/src/Stanza/Vcard4.php
+++ lib/moxl/src/Stanza/Vcard4.php
@@ -91,6 +91,35 @@ class Vcard4
             $vcard->appendChild($adr);
         }
 
+        $publishOption = $dom->createElement('publish-option');
+        $x = $dom->createElement('x');
+        $x->setAttribute('xmlns', 'jabber:x:data');
+        $x->setAttribute('type', 'submit');
+        $publishOption->appendChild($x);
+
+        $field = $dom->createElement('field');
+        $field->setAttribute('var', 'FORM_TYPE');
+        $field->setAttribute('type', 'hidden');
+        $field->appendChild($dom->createElement('value', 'http://jabber.org/protocol/pubsub#publish-options'));
+        $x->appendChild($field);
+
+        $field = $dom->createElement('field');
+        $field->setAttribute('var', 'pubsub#persist_items');
+        $field->appendChild($dom->createElement('value', 'true'));
+        $x->appendChild($field);
+
+        $field = $dom->createElement('field');
+        $field->setAttribute('var', 'pubsub#send_last_published_item');
+        $field->appendChild($dom->createElement('value', 'on_sub_and_presence'));
+        $x->appendChild($field);
+
+        $field = $dom->createElement('field');
+        $field->setAttribute('var', 'pubsub#deliver_payloads');
+        $field->appendChild($dom->createElement('value', 'true'));
+        $x->appendChild($field);
+
+        $pubsub->appendChild($publishOption);
+
         $xml = \Moxl\API::iqWrapper($pubsub, false, 'set');
         \Moxl\API::request($xml);
     }
