83c4bbf18029f7dcafc642afbc25b3f7b636e51c
Timothée Jaussoin <edhelas@movim.eu>
2023-03-01 23:40:57 +0100

    Use a hash for the cached templates, to prevent some filename limitation

--- src/Movim/Template/Partial.php
+++ src/Movim/Template/Partial.php
@@ -76,6 +76,6 @@ class Partial extends Tpl
 
     private function resolvedCacheKey(string $templateFilePath, string $key): string
     {
-        return CACHE_PATH . sha1(User::me()->id) . '_' . $templateFilePath . '_' . cleanupId($key). $this->extension;
+        return CACHE_PATH . sha1(User::me()->id) . '_' . $templateFilePath . '_' . hash('sha256', $key). $this->extension;
     }
 }
