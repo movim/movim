movim/movim

commit dfdefbef8f4e75de34b4a57d18a84fdd0825435e

Make Chat::ajaxGetRoom a non blocking request

app/widgets/Chat/Chat.php
@@ -262,7 +262,7 @@ class Chat extends \Movim\Widget\Base
 
     public function onConferenceSubject($packet)
     {
-        $this->ajaxGetRoom($packet->content->jidfrom, false, true);
+        $this->ajaxHttpGetRoom($packet->content->jidfrom, false, true);
     }
 
     public function onMAMRetrieved()
@@ -272,7 +272,7 @@ class Chat extends \Movim\Widget\Base
 
     public function onMAMMucRetrieved($packet)
     {
-        $this->ajaxGetRoom($packet->content, true, true);
+        $this->ajaxHttpGetRoom($packet->content, true, true);
     }
 
     public function onMAMContactRetrieved($packet)
@@ -285,7 +285,7 @@ class Chat extends \Movim\Widget\Base
         list($content, $notify) = $packet->content;
 
         if ($notify) {
-            $this->ajaxGetRoom($content->jid, false, true);
+            $this->ajaxHttpGetRoom($content->jid, false, true);
         }
     }
 
@@ -417,7 +417,7 @@ class Chat extends \Movim\Widget\Base
      * @brief Get a chatroom
      * @param string $jid
      */
-    public function ajaxGetRoom(string $room, $light = false, $noConnect = false)
+    public function ajaxHttpGetRoom(string $room, $light = false, $noConnect = false)
     {
         if (!validateJid($room)) {
             return;
@@ -960,7 +960,7 @@ class Chat extends \Movim\Widget\Base
      *
      * @param string $room
      */
-    public function ajaxGetRoomConfig($room)
+    public function ajaxHttpGetRoomConfig($room)
     {
         if (!validateJid($room)) {
             return;

app/widgets/Chat/_chat_header.tpl
@@ -128,7 +128,7 @@
                 </li>
             {/if}
             {if="$conference->presence->mucaffiliation == 'owner'"}
-                <li onclick="Chat_ajaxGetRoomConfig('{$jid|echapJS}')">
+                <li onclick="Chat_ajaxHttpGetRoomConfig('{$jid|echapJS}')">
                     <div>
                         <p class="normal">{$c->__('chatroom.administration')}</p>
                     </div>

app/widgets/Chat/chat.js
@@ -133,7 +133,7 @@ var Chat = {
         var chat = document.querySelector('#chat_widget');
         chat.innerHTML = '';
 
-        Chat_ajaxGetRoom(jid);
+        Chat_ajaxHttpGetRoom(jid);
     },
     sendMessage: function () {
         var textarea = Chat.getTextarea();

app/widgets/ChatOmemo/chatomemo.js
@@ -134,7 +134,7 @@ var ChatOmemo = {
 
                 // Refreshing the unencrypted messages
                 if (Boolean(textarea.dataset.muc)) {
-                    Chat_ajaxGetRoom(jid);
+                    Chat_ajaxHttpGetRoom(jid);
                 } else {
                     Chat_ajaxGet(jid);
                 }
@@ -359,7 +359,7 @@ var ChatOmemo = {
         store.setContactState(jid, true);
 
         if (muc) {
-            Chat_ajaxGetRoom(jid);
+            Chat_ajaxHttpGetRoom(jid);
         } else {
             Chat_ajaxGet(jid);
         }

commit 04dc82b6312bcd586422d424f7e8d05e479a0f8a

Fix a naming issue introduced by dfdefbef8f4e75de34b4a57d18a84fdd0825435e

app/widgets/Chat/Chat.php
@@ -960,7 +960,7 @@ class Chat extends \Movim\Widget\Base
      *
      * @param string $room
      */
-    public function ajaxHttpGetRoomConfig($room)
+    public function ajaxGetRoomConfig($room)
     {
         if (!validateJid($room)) {
             return;

app/widgets/Chat/_chat_header.tpl
@@ -128,7 +128,7 @@
                 </li>
             {/if}
             {if="$conference->presence->mucaffiliation == 'owner'"}
-                <li onclick="Chat_ajaxHttpGetRoomConfig('{$jid|echapJS}')">
+                <li onclick="Chat_ajaxGetRoomConfig('{$jid|echapJS}')">
                     <div>
                         <p class="normal">{$c->__('chatroom.administration')}</p>
                     </div>