movim/movim

commit 0d9854cbef3f0267dd34b4375a036af7f7721fcc

Treat the post title as a content if there is no content available Remove the fix versions in composer.json

app/Post.php
@@ -393,19 +393,18 @@ class Post extends Model
             $this->aname = null;
         }
 
-        $this->title = $this->extractTitle($entry->entry->title);
-
         // Content
         $summary = ($entry->entry->summary && (string)$entry->entry->summary != '')
             ? '<p class="summary">'.(string)$entry->entry->summary.'</p>'
             : null;
 
-        if ($entry->entry && $entry->entry->content) {
+        $content = '';
+
+        if ($entry->entry->content) {
             $content = $this->extractContent($entry->entry->content);
-        } elseif ($summary == '') {
-            $content = __('');
+            $this->title = $this->extractTitle($entry->entry->title);
         } else {
-            $content = '';
+            $content = $this->extractContent($entry->entry->title);
         }
 
         $content = $summary.$content;
@@ -710,7 +709,7 @@ class Post extends Model
 
     public function isRTL()
     {
-        return (isRTL($this->contentraw ?? '') || isRTL($this->title));
+        return (isRTL($this->contentraw ?? '') || isRTL($this->title ?? ''));
     }
 
     public function isComment()
@@ -735,15 +734,15 @@ class Post extends Model
 
     public function getTitle()
     {
-        return (isset($this->title)
-            && strlen($this->title) >= $this->titleLimit)
+        return ($this->title == null
+            || strlen($this->title) >= $this->titleLimit)
             ? __('post.default_title')
             : $this->title;
     }
 
     public function getContent()
     {
-        return (strlen($this->title) >= $this->titleLimit)
+        return ($this->title && strlen($this->title) >= $this->titleLimit)
             ? nl2br(addUrls(addHashtagsLinks($this->title)))
             : $this->contentcleaned;
     }

commit 413f416b28f5f31442f3be05acca6b697d624c59

Fix comments and likes issue introduced by 0d9854cbef3f0267dd34b4375a036af7f7721fcc

app/Post.php
@@ -704,7 +704,7 @@ class Post extends Model
 
     public function isLike()
     {
-        return ($this->contentraw == '♥' || $this->title == '♥');
+        return ($this->contentraw == '♥' || $this->content == '♥');
     }
 
     public function isRTL()

app/widgets/Post/_post_comments.tpl
@@ -39,7 +39,7 @@
     {/if}
 
     {loop="$post->comments"}
-        {if="$value->title || $value->contentraw"}
+        {if="$value->content || $value->contentraw"}
         <li id="{$value->nodeid|cleanupId}"
             {if="!$public && $value->isMine(true) && $value->isLike()"}class="mine"{/if}>
             {if="!$public && ($value->isMine() || $post->isMine())"}
@@ -93,7 +93,7 @@
                         {autoescape/}
                     {else}
                         {autoescape="off"}
-                            {$value->title|addUrls|addHashtagsLinks|nl2br|prepareString}
+                            {$value->content|addUrls|addHashtagsLinks|nl2br|prepareString}
                         {/autoescape}
                     {/if}
                 </p>