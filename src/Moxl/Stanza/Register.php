054e2901e07be5697df7ed49c608303fc27e84f8
Timothée Jaussoin <edhelas@movim.eu>
2024-02-26 20:02:37 +0100

    Fix basic account registration, see https://xmpp.org/extensions/xep-0077.html#usecases-register

--- src/Moxl/Stanza/Register.php
+++ src/Moxl/Stanza/Register.php
@@ -17,12 +17,19 @@ class Register
         $dom = new \DOMDocument('1.0', 'UTF-8');
         $query = $dom->createElementNS('jabber:iq:register', 'query');
 
-        $x = $dom->createElement('x');
-        $x->setAttribute('xmlns', 'jabber:x:data');
-        $x->setAttribute('type', 'submit');
-        $query->appendChild($x);
+        // https://xmpp.org/extensions/xep-0077.html#usecases-register
+        if (substr(array_key_first($data), 0, 8) == 'generic_') {
+            foreach ($data as $key => $value) {
+                $query->appendChild($dom->createElement(substr($key, 8), $value));
+            }
+        } else {
+            $x = $dom->createElement('x');
+            $x->setAttribute('xmlns', 'jabber:x:data');
+            $x->setAttribute('type', 'submit');
+            $query->appendChild($x);
 
-        \Moxl\Utils::injectConfigInX($x, $data);
+            \Moxl\Utils::injectConfigInX($x, $data);
+        }
 
         \Moxl\API::request(\Moxl\API::iqWrapper($query, $to, 'set'));
     }
