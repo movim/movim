4cd482518cdbb049882803ce8b841a55688dc85d
Timothée Jaussoin <edhelas@movim.eu>
2025-05-02 09:33:09 +0200

    Compress a bit more the avatars to let them pass the size limitation

--- public/scripts/movim_avatar.js
+++ public/scripts/movim_avatar.js
@@ -32,7 +32,7 @@ var MovimAvatar = {
             MovimUtils.applyOrientation(ctx, orientation, width, height)
             MovimUtils.drawImageProp(ctx, image);
 
-            var base64 = canvas.toDataURL('image/jpeg', 0.95);
+            var base64 = canvas.toDataURL('image/jpeg', 0.90);
 
             var preview = document.querySelector('form[name=' + formname + '] img');
 
