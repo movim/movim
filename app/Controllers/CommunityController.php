90826b91e816e430b3aa2f02a3a4def135716cd8
Timothée Jaussoin <edhelas@movim.eu>
2025-05-21 13:53:57 +0200

    Redirect to the post page when authenticated and community URL is having a nodeid

--- app/Controllers/CommunityController.php
+++ app/Controllers/CommunityController.php
@@ -9,5 +9,13 @@ class CommunityController extends Base
     public function dispatch()
     {
         $this->page->setTitle(__('page.explore'));
+
+        if (isLogged() && $this->fetchGet('i')) {
+            $this->redirect('post', [
+                's' => $this->fetchGet('s'),
+                'n' => $this->fetchGet('n'),
+                'i' => $this->fetchGet('i'),
+            ]);
+        }
     }
 }
