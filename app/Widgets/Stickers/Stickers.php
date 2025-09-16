5654f01cda4118ff77b6159f96240aea8db0f7cb
Timothée Jaussoin <edhelas@movim.eu>
2025-09-16 09:43:30 +0200

    Handle XEP-0447: Stateless file sharing disposition attribute if present and use it to display inline attachments in the chat

--- app/Widgets/Stickers/Stickers.php
+++ app/Widgets/Stickers/Stickers.php
@@ -15,8 +15,8 @@ use App\Sticker;
 use App\StickersPack;
 use App\Widgets\Chat\Chat;
 use App\Widgets\Chats\Chats;
-use App\Widgets\Dialog\Dialog;
 use App\Widgets\Drawer\Drawer;
+use Moxl\Xec\Payload\Packet;
 
 class Stickers extends \Movim\Widget\Base
 {
