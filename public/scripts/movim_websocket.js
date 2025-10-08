0f721b6d746915ca6905782550f27a7a0c72e8b0
Timothée Jaussoin <edhelas@movim.eu>
2025-10-08 19:01:28 +0200

    Reduce the WS reconnection max time to 10s Fix the list active CSS

--- public/scripts/movim_websocket.js
+++ public/scripts/movim_websocket.js
@@ -217,8 +217,8 @@ var MovimWebsocket = {
     generateInterval: function () {
         var maxInterval = (Math.pow(2, MovimWebsocket.attempts) - 1) * 1000;
 
-        if (maxInterval > 30 * 1000) {
-            maxInterval = 30 * 1000; // If the generated interval is more than 30 seconds, truncate it down to 30 seconds.
+        if (maxInterval > 10 * 1000) {
+            maxInterval = 10 * 1000; // If the generated interval is more than 30 seconds, truncate it down to 30 seconds.
         }
 
         // generate the interval to a random number between 0 and the maxInterval determined from above
