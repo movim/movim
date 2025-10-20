86bbb55f6066dca0d4d25d7613bacbbec0565b7d
Timothée Jaussoin <edhelas@movim.eu>
2025-10-20 20:37:46 +0200

    Replace the array of emoji matching regex with a unique and universal one

--- src/Movim/Emoji.php
+++ src/Movim/Emoji.php
@@ -34,35 +34,6 @@ class Emoji
     private $_lastEmoji = null;
     private $_lastEmojiUrl = null;
     private $_lastEmojiTitle = null;
-    private $_regex = [
-        // Some easy cases first
-        '/[#*0-9]\x{20E3}
-         |\x{1F3F3}(?:\x{FE0F}\x{200D}\x{1F308}|\x{FE0F}\x{200D}\x{26A7}\x{FE0F})?
-         |\x{1F3F4}(?:\x{200D}\x{2620}\x{FE0F}|\x{E0067}\x{E0062}
-          (?:\x{E0065}\x{E006E}\x{E0067}|\x{E0073}\x{E0063}\x{E0074}|\x{E0077}\x{E006C}\x{E0073})\x{E007F})?
-         |\x{1F441}(?:\x{200D}\x{1F5E8})?
-         /ux',
-        // Everything starting with 1F468 or 1F469
-        '/[\x{1F468}\x{1F469}]
-          (?:\x{200D}\x{2764}\x{FE0F}\x{200D}(?:\x{1F48B}\x{200D})?[\x{1F468}\x{1F469}]
-            |(?:\x{200D}[\x{1F468}\x{1F469}])?
-             (?:\x{200D}[\x{1F466}\x{1F467}])?
-             \x{200D}[\x{1F466}\x{1F467}]
-            |[\x{1F3FB}-\x{1F3FF}]?\x{200D}
-             (?:[\x{2695}\x{2696}\x{2708}]\x{FE0F}
-               |[\x{1F33E}\x{1F373}\x{1F393}\x{1F3A4}\x{1F3A8}\x{1F3EB}\x{1F3ED}\x{1F4BB}\x{1F4BC}\x{1F527}\x{1F52C}\x{1F680}\x{1F692}]
-             )
-          )/ux',
-        // Some more combinations (order is important!)
-        '/[\x{26F9}\x{1F3C3}-\x{1F3CC}\x{1F46E}\x{1F46F}\x{1F461}-\x{1F477}\x{1F481}-\x{1F487}\x{1F575}\x{1F645}-\x{1F64E}\x{1F6A3}\x{1F6B4}-\x{1F6B6}\x{1F926}\x{1F937}-\x{1F93E}\x{1F9D6}-\x{1F9DF}\x{1F979}\x{1F9CC}]
-          [\x{FE0F}\x{1F3FB}-\x{1F3FF}]?
-          \x{200D}[\x{2640}\x{2642}]\x{FE0F}
-         /ux',
-        '/[\x{261D}\x{26F7}-\x{270D}\x{1F1E6}-\x{1F1FF}\x{1F385}\x{1F3C2}-\x{1F3CC}\x{1F442}-\x{1F487}\x{1F4AA}\x{1F574}-\x{1F596}\x{1F645}-\x{1F6CC}\x{1F918}-\x{1F9DD}\x{1F6DD}-\x{1F6DF}\x{1F7F0}]
-          [\x{1F1E6}-\x{1F1FF}\x{1F3FB}-\x{1F3FF}]/ux',
-        // Individual codepoints last
-        '/[\x{203C}\x{2049}\x{2139}-\x{21AA}\x{231A}-\x{23FA}\x{24C2}\x{25AA}-\x{27BF}\x{2934}-\x{2B55}\x{3030}-\x{3299}\x{1F004}-\x{1F9FF}\x{1FAAA}-\x{1FAFF}]/u'
-    ];
 
     protected function __construct()
     {
@@ -78,37 +49,40 @@ class Emoji
     {
         // Remove the Variation Selectors (Unicode block) for a proper comparison
         $this->_string = preg_replace('/[\x{fe00}\x{fe0f}]/u', '', $string);
-        //$this->_string = $string;
         $this->_lastEmoji = null;
 
-        $replaced = preg_replace_callback($this->_regex, function ($matches) use ($noTitle) {
-            $astext = implode(
-                '-',
-                array_map(
-                    'dechex',
-                    unpack('N*', mb_convert_encoding($matches[0], 'UCS-4BE', 'UTF-8'))
-                )
-            );
+        $replaced = preg_replace_callback(
+            '/(?:\p{Extended_Pictographic}[\p{Emoji_Modifier}\p{M}]*(?:\p{Join_Control}\p{Extended_Pictographic}[\p{Emoji_Modifier}\p{M}]*)*|\s|.)\p{M}*/u',
+            function ($matches) use ($noTitle) {
+                $astext = implode(
+                    '-',
+                    array_map(
+                        'dechex',
+                        unpack('N*', mb_convert_encoding($matches[0], 'UCS-4BE', 'UTF-8'))
+                    )
+                );
 
-            if (!isset($this->_emoji[$astext])) {
-                return $matches[0];
-            }
+                if (!isset($this->_emoji[$astext])) {
+                    return $matches[0];
+                }
 
-            $this->_lastEmoji = $matches[0];
-            $this->_lastEmojiUrl = BASE_URI . 'theme/img/emojis/svg/' . $astext . '.svg';
+                $this->_lastEmoji = $matches[0];
+                $this->_lastEmojiUrl = BASE_URI . 'theme/img/emojis/svg/' . $astext . '.svg';
 
-            $dom = new \DOMDocument('1.0', 'UTF-8');
-            $dom->appendChild($img = $dom->createElement('img'));
-            $img->setAttribute('class', 'emoji');
-            $img->setAttribute('alt', $this->_emoji[$astext]);
-            if (!$noTitle) {
-                $this->_lastEmojiTitle = \emojiShortcut($this->_emoji[$astext]);
-                $img->setAttribute('title', ':' . $this->_lastEmojiTitle . ':');
-            }
-            $img->setAttribute('src', $this->_lastEmojiUrl);
+                $dom = new \DOMDocument('1.0', 'UTF-8');
+                $dom->appendChild($img = $dom->createElement('img'));
+                $img->setAttribute('class', 'emoji');
+                $img->setAttribute('alt', $this->_emoji[$astext]);
+                if (!$noTitle) {
+                    $this->_lastEmojiTitle = \emojiShortcut($this->_emoji[$astext]);
+                    $img->setAttribute('title', ':' . $this->_lastEmojiTitle . ':');
+                }
+                $img->setAttribute('src', $this->_lastEmojiUrl);
 
-            return $dom->saveXML($dom->documentElement);
-        }, $string);
+                return $dom->saveXML($dom->documentElement);
+            },
+            $string
+        );
 
         return $replaced ?? $string;
     }
