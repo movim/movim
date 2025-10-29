9d5c79166e65508b4a8807a193f02ee62a4b7ac5
Timothée Jaussoin <edhelas@movim.eu>
2025-10-29 13:27:54 +0100

    Fix #1482 Return a proper service-unavailable when Movim cannot reply a IQ request

--- src/Moxl/Stanza/Iq.php
+++ src/Moxl/Stanza/Iq.php
@@ -6,6 +6,12 @@ class Iq
 {
     public static function error(string $to, string $id)
     {
-        \Moxl\API::request(\Moxl\API::iqWrapper(null, $to, 'error', $id));
+        $dom = new \DOMDocument('1.0', 'UTF-8');
+        $error = $dom->createElement('error');
+        $error->setAttribute('type', 'cancel');
+
+        $error->appendChild($dom->createElementNS('urn:ietf:params:xml:ns:xmpp-stanzas', 'service-unavailable'));
+
+        \Moxl\API::request(\Moxl\API::iqWrapper($error, $to, 'error', $id));
     }
 }
