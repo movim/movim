c39ab50a6f4e392fa1b9748106f1758dc0182869
Timothée Jaussoin <edhelas@movim.eu>
2025-11-29 20:52:26 +0100

    Update the Manifest icons

--- app/Widgets/Manifest/Manifest.php
+++ app/Widgets/Manifest/Manifest.php
@@ -14,12 +14,17 @@ class Manifest extends Base
             'description'   => __('global.description'),
             'scope'         => BASE_URI,
             'icons'         => [
-                /*[
-                    'src' => BASE_URI . 'theme/img/app/vectorial_square.svg',
-                    'sizes' => '512x512',
-                    'type' => 'image/svg+xml',
+                [
+                    'src' => BASE_URI . 'theme/img/app/1024_square.png',
+                    'sizes' => '1024x1024',
+                    'type' => 'image/png',
                     'purpose' => 'maskable'
-                ],*/
+                ],
+                [
+                    'src' => BASE_URI . 'theme/img/app/1024.png',
+                    'sizes' => '1024x1024',
+                    'type' => 'image/png'
+                ],
                 [
                     'src' => BASE_URI . 'theme/img/app/512_square.png',
                     'sizes' => '512x512',
@@ -29,20 +34,38 @@ class Manifest extends Base
                 [
                     'src' => BASE_URI . 'theme/img/app/512.png',
                     'sizes' => '512x512',
+                    'type' => 'image/png'
+                ],
+                [
+                    'src' => BASE_URI . 'theme/img/app/192_square.png',
+                    'sizes' => '192x192',
                     'type' => 'image/png',
-                    'purpose' => 'any'
+                    'purpose' => 'maskable'
                 ],
                 [
                     'src' => BASE_URI . 'theme/img/app/192.png',
                     'sizes' => '192x192',
-                    'type' => 'image/png',
-                    'purpose' => 'any'
+                    'type' => 'image/png'
+                ],
+                [
+                    'src' => BASE_URI . 'theme/img/app/144.png',
+                    'sizes' => '144x144',
+                    'type' => 'image/png'
                 ],
                 [
                     'src' => BASE_URI . 'theme/img/app/96.png',
                     'sizes' => '96x96',
-                    'type' => 'image/png',
-                    'purpose' => 'any'
+                    'type' => 'image/png'
+                ],
+                [
+                    'src' => BASE_URI . 'theme/img/app/72.png',
+                    'sizes' => '72x72',
+                    'type' => 'image/png'
+                ],
+                [
+                    'src' => BASE_URI . 'theme/img/app/48.png',
+                    'sizes' => '48x48',
+                    'type' => 'image/png'
                 ]
             ],
             'display_override' => ['window-controls-overlay'],
