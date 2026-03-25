3c038f90a596ed3fc07eeb5d02a10f9bcedf3897
Timothée Jaussoin <edhelas@movim.eu>
2026-03-25 18:15:45 +0100

    Don't apply the Notifications to the Spaces counters

--- app/Widgets/Notifications/notifications.js
+++ app/Widgets/Notifications/notifications.js
@@ -1,6 +1,6 @@
 var Notifications = {
     setCounters : function(counter) {
-        var counters = document.querySelectorAll('.counter.notifications');
+        var counters = document.querySelectorAll('.counter.notifications:not([data-key])');
         for (i = 0; i < counters.length; i++) {
             counters[i].innerHTML = counter;
         }
