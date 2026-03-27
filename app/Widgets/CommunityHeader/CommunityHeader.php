cc838550ab434d914d46b93f2d80f116d7124602
Timothée Jaussoin <edhelas@movim.eu>
2026-03-27 11:41:14 +0100

    Fix crash on discovery in CommunityHeader
    Refactor the class, and rename some variables

--- app/Widgets/CommunityHeader/CommunityHeader.php
+++ app/Widgets/CommunityHeader/CommunityHeader.php
@@ -12,6 +12,7 @@ use Moxl\Xec\Action\PubsubSubscription\Add as SubscriptionAdd;
 use Moxl\Xec\Action\PubsubSubscription\Remove as SubscriptionRemove;
 use Moxl\Xec\Action\Pubsub\TestPostPublish;
 use Moxl\Xec\Payload\Packet;
+use stdClass;
 
 class CommunityHeader extends Base
 {
@@ -32,7 +33,11 @@ class CommunityHeader extends Base
     {
         $info = $packet->content;
 
-        if (!$info->isMicroblogCommentsNode() && !$info->isAccount()) {
+        if (
+            $info->identities->contains('category', 'pubsub')
+            && $info->identities->contains('type', 'leaf')
+            && !$info->isMicroblogCommentsNode()
+        ) {
             $this->rpc('MovimTpl.fill', '#community_header', $this->prepareHeader($info->server, $info->node));
         }
     }
@@ -57,10 +62,10 @@ class CommunityHeader extends Base
 
     public function onSubscribed(Packet $packet)
     {
-        list($origin, $node) = array_values($packet->content);
+        list($server, $node) = array_values($packet->content);
 
         if ($node != Post::MICROBLOG_NODE) {
-            $this->ajaxGetMetadata($origin, $node);
+            $this->ajaxGetMetadata($server, $node);
         }
 
         $this->toast($this->__('communityheader.followed'));
@@ -73,51 +78,51 @@ class CommunityHeader extends Base
 
     public function onUnsubscribed(Packet $packet)
     {
-        list($origin, $node) = array_values($packet->content);
+        list($server, $node) = array_values($packet->content);
 
         if ($node != Post::MICROBLOG_NODE) {
-            $this->ajaxGetMetadata($origin, $node);
+            $this->ajaxGetMetadata($server, $node);
         }
 
         $this->toast($this->__('communityheader.unfollowed'));
     }
 
-    public function ajaxGetMetadata($origin, $node)
+    public function ajaxGetMetadata(string $server, string $node)
     {
-        if (!validateServerNode($origin, $node)) {
+        if (!validateServerNode($server, $node)) {
             return;
         }
 
         $r = $this->xmpp(new Request);
-        $r->setTo($origin)->setNode($node)
+        $r->setTo($server)->setNode($node)
             ->request();
     }
 
-    public function ajaxAskSubscribe($origin, $node)
+    public function ajaxAskSubscribe(string $server, string $node)
     {
-        if (!validateServerNode($origin, $node)) {
+        if (!validateServerNode($server, $node)) {
             return;
         }
 
         $view = $this->tpl();
 
-        $view->assign('server', $origin);
+        $view->assign('server', $server);
         $view->assign('node', $node);
-        $view->assign('info', \App\Info::where('server', $origin)
+        $view->assign('info', \App\Info::where('server', $server)
             ->where('node', $node)
             ->first());
 
         $this->dialog($view->draw('_communityheader_subscribe'));
     }
 
-    public function ajaxSubscribe($form, $origin, $node)
+    public function ajaxSubscribe(stdClass $form, string $server, string $node)
     {
-        if (!validateServerNode($origin, $node)) {
+        if (!validateServerNode($server, $node)) {
             return;
         }
 
         $g = $this->xmpp(new Subscribe);
-        $g->setTo($origin)
+        $g->setTo($server)
             ->setNode($node)
             ->setFrom($this->me->id)
             ->setData(formToArray($form))
@@ -125,44 +130,44 @@ class CommunityHeader extends Base
 
         if ($form->share->value) {
             $a = $this->xmpp(new SubscriptionAdd);
-            $a->setServer($origin)
+            $a->setServer($server)
                 ->setNode($node)
                 ->setFrom($this->me->id)
                 ->request();
         }
     }
 
-    public function ajaxAskUnsubscribe($origin, $node)
+    public function ajaxAskUnsubscribe(string $server, string $node)
     {
-        if (!validateServerNode($origin, $node)) {
+        if (!validateServerNode($server, $node)) {
             return;
         }
 
         $view = $this->tpl();
 
-        $view->assign('server', $origin);
+        $view->assign('server', $server);
         $view->assign('node', $node);
-        $view->assign('info', \App\Info::where('server', $origin)
+        $view->assign('info', \App\Info::where('server', $server)
             ->where('node', $node)
             ->first());
 
         $this->dialog($view->draw('_communityheader_unsubscribe'));
     }
 
-    public function ajaxUnsubscribe($origin, $node)
+    public function ajaxUnsubscribe(string $server, string $node)
     {
-        if (!validateServerNode($origin, $node)) {
+        if (!validateServerNode($server, $node)) {
             return;
         }
 
         $subscriptions = $this->me->subscriptions()
-            ->where('server', $origin)
+            ->where('server', $server)
             ->where('node', $node)
             ->get();
 
         foreach ($subscriptions as $s) {
             $g = $this->xmpp(new Unsubscribe);
-            $g->setTo($origin)
+            $g->setTo($server)
                 ->setNode($node)
                 ->setSubid($s->subid)
                 ->setFrom($this->me->id)
@@ -170,7 +175,7 @@ class CommunityHeader extends Base
         }
 
         $r = $this->xmpp(new SubscriptionRemove);
-        $r->setServer($origin)
+        $r->setServer($server)
             ->setNode($node)
             ->setFrom($this->me->id)
             ->request();
@@ -179,40 +184,40 @@ class CommunityHeader extends Base
     /*
      * Sic, doing this hack and wait to have a proper way to test it in the standard
      */
-    public function ajaxTestPublish($origin, $node)
+    public function ajaxTestPublish(string $server, string $node)
     {
-        if (!validateServerNode($origin, $node)) {
+        if (!validateServerNode($server, $node)) {
             return;
         }
 
         $t = $this->xmpp(new TestPostPublish);
-        $t->setTo($origin)
+        $t->setTo($server)
             ->setNode($node)
             ->request();
     }
 
-    public function prepareHeader($origin, $node)
+    public function prepareHeader(string $server, string $node)
     {
         $view = $this->tpl();
 
-        $info = \App\Info::where('server', $origin)
+        $info = \App\Info::where('server', $server)
             ->where('node', $node)
             ->first();
 
         $view->assign('info', $info);
         $view->assign('subscription', $this->me->subscriptions()
-            ->where('server', $origin)
+            ->where('server', $server)
             ->where('node', $node)
             ->first());
         $view->assign('num', $info ?
             ($info->items > 0)
             ? $info->items
-            : \App\Post::where('server', $origin)
+            : \App\Post::where('server', $server)
             ->where('node', $node)
             ->count()
             : 0);
         $view->assign('node', $node);
-        $view->assign('server', $origin);
+        $view->assign('server', $server);
 
         return $view->draw('_communityheader');
     }
